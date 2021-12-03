1ee3d2b7cd219f83882535e6f55c5e494718dda5
MigBash <20924663+migbash@users.noreply.github.com>
2021-12-03 04:52:08 +0000

    adding PWA support to the sveltekit application (#52)

---
+++ src/lib/components/Offline_alert.svelte
@@ -0,0 +1,32 @@
+<!-- ===================
+	COMPONENT JS - BASIC 
+    [TypeScript Written]
+=================== -->
+
+<script lang="ts">
+
+</script>
+
+<!-- ===================
+	COMPONENT HTML
+=================== -->
+
+<div id='offline-alert-container'>
+    <p class='s-12 color-white'>
+        Warning there is no connection to the internet you are using the website in offline mode
+    </p>
+</div>
+
+<!-- ===================
+	COMPONENT STYLE
+=================== -->
+
+<style>
+    #offline-alert-container {
+        position: relative;
+        width: 100%;
+        padding: 8px 27px;
+        background: #FF3C3C;
+        text-align: center;
+    }
+</style>
