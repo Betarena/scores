7a7b84e954602997ad247e9c4b1638ebc6876c04
migbash <20924663+migbash@users.noreply.github.com>
2022-03-07 06:18:47 +0000

    issue #252 for the newsletter

--- src/lib/components/footer/_Footer.svelte
+++ src/lib/components/footer/_Footer.svelte
@@ -8,6 +8,7 @@
 	import { onMount } from 'svelte'
 	import { page } from '$app/stores'
 	import { browser, dev } from '$app/env'
+  import { fade } from 'svelte/transition'
 	// ... typescript-types;
 	import type { Footer_Data } from '$lib/models/footer/types'
 	// ... image-assets;
@@ -88,12 +89,32 @@
 	 * ... form SUBMIT method to 
 	 * ... register user on the BETARENA EMAIL LIST
 	*/
+  let showEmailForm: boolean = false;
+  // ...
 	async function submitEmail() {
+    // ... DEBUGGING;
 		if (dev) console.debug('subscribing to email newsletter!')
+    // ...
+    showEmailForm = true;
+    // ...
+    // setTimeout(async () => {
+    //   // ... IFRAME CSS STYLING;
+    //   let iFrameEmailFormElement = document.getElementById('iframe-email-form');
+    //   // let doc = iFrameEmailFormElement.ownerDocument;
+    //   let doc = (iFrameEmailFormElement.contentWindow || iFrameEmailFormElement.contentDocument);
+    //   // if (doc.document)doc = doc.document
+    //   // ... DEBUGGING;
+    //   if (dev) console.debug('iFrameEmailFormElement', iFrameEmailFormElement);
+    //   if (dev) console.debug('doc', doc.document);
+    //   // ...
+    //   // doc.body.style.backgroundColor = "red";
+    //   // ... 
+    // }, 5000)
 	}
 
   // ... assign `logo-link`;
   let logoLink: string
+  // ... [REACTIVITY] - METHOD;
   $: if (server_side_language != 'en') {
     logoLink = $page.url.origin + '/' + server_side_language
   } else {
@@ -102,12 +123,12 @@
 
   // ... hide SEO on-load;
   let hideSEO: boolean = false;
-  // ...
+  // ... [REACTIVITY] - METHOD;
   $: if (browser) {
     hideSEO = true
   }
 
-  	// ... reload-PAGE-check;
+  // ... reload-PAGE-check;
 	function reloadPage() {
 		// ... DEBUGGING;
 		if (dev) console.debug('reloading-page-method!')
@@ -768,6 +789,45 @@
 </footer>
 
 
+<!-- ... email-submission-send-grid-form ... -->
+{#if showEmailForm}
+  <div 
+    id='background-modal-blur'
+    on:click={() => showEmailForm = false}
+    in:fade />
+
+  <!-- ... IFRAME LANGUAGE CONDITION ... -->
+  {#if server_side_language == 'en'}
+    <!-- ... EN ... -->
+    <iframe
+      id='iframe-email-form'
+      title='email-betarena-form'
+      src="https://cdn.forms-content.sg-form.com/959f35f0-8a5e-11ec-ae12-6ab0b90e93b0" />
+  {:else if server_side_language == 'es'}
+    <!-- ... ES ... -->
+    <iframe 
+      title='email-betarena-form'
+      src="https://cdn.forms-content.sg-form.com/e2e80a33-947e-11ec-9a6f-bab7bbd1e44a" />
+  {:else if server_side_language == 'it'}
+    <!-- ... IT ... -->
+    <iframe 
+      title='email-betarena-form'
+      src="https://cdn.forms-content.sg-form.com/f6daf19c-947f-11ec-9a6f-bab7bbd1e44a" />
+  {:else if server_side_language == 'br'}
+    <!-- ... BR ... -->
+    <iframe 
+      title='email-betarena-form'
+      src="https://cdn.forms-content.sg-form.com/6a180301-9480-11ec-9a6f-bab7bbd1e44a" />
+  {:else if server_side_language == 'pt'}
+    <!-- ... PT ... -->
+    <iframe 
+      title='email-betarena-form'
+      src="https://cdn.forms-content.sg-form.com/ae8bbec3-9480-11ec-9a6f-bab7bbd1e44a" />
+  {/if}
+
+{/if}
+
+
 <!-- ===================
 	COMPONENT HTML
 =================== -->
@@ -830,6 +890,40 @@
 		box-shadow: 0px 3px 8px rgba(212, 84, 12, 0.32);
 		border-radius: 8px;
 	}
+
+  div#background-modal-blur {
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: 4;
+    height: 100%;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    backdrop-filter: blur(4px);
+  }
+
+  /* import-form */
+  iframe {
+    position: fixed;
+    right: 0;
+    left: 0;
+    margin: auto;
+    top: 0;
+    bottom: 0;
+    z-index: 5;
+    /*  */
+    width: 469px;
+    height: 597px;
+    border: none;
+  }
+  /* iframe body {
+    margin: 0 !important;
+  }
+  iframe form {
+    width: auto !important;
+    margin: 0 !important;
+  } */
 
 	/* 
     RESPONSIVE FOR TABLET (&+) [768px] */
