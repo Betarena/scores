661658de7def14313bbd51bde804804fbc5240c5
migbash <20924663+migbash@users.noreply.github.com>
2022-03-24 18:20:03 +0000

    updating svg correctly

--- src/lib/components/best_goalscorers/loaders/_Placeholder_Table_Row.svelte
+++ src/lib/components/best_goalscorers/loaders/_Placeholder_Table_Row.svelte
@@ -5,10 +5,12 @@
 <svg 
 	  class="animation-target"
     width="100%" height="32" 
-    viewBox="0 0 100% 32" 
+    viewBox="0 0 462 32" 
     fill="none"
     xmlns="http://www.w3.org/2000/svg">
     
     <rect width="100%" height="32" rx="2" style={$userBetarenaSettings.theme == 'Dark' ? 'fill: #616161;' : 'fill: #F2F2F2;'} />
 
 </svg>
+
+<!-- viewBox="0 0 462 32"  -->
