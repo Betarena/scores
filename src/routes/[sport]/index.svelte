bfb4bbbf4c0373d24a91a864e8f389e114bacf94
migbash <20924663+migbash@users.noreply.github.com>
2022-06-06 20:50:16 +0100

    adding a dedicated sport dynamic routes for EN only

---
+++ src/routes/[sport]/index.svelte
@@ -0,0 +1,27 @@
+<!-- 
+=================
+    PRE-COMPONENT JS SCRIPT,
+    PRE-LOADING CRITICAL COMPONENT DATA,
+    #####
+    -> pre-loading `{sport}-page data;
+=================
+-->
+<script lang="ts" context="module">
+
+	/** 
+	 * @type {import('@sveltejs/kit').Load} 
+	*/
+	export async function load({ 
+    url, 
+    params, 
+    fetch
+  }) {
+
+		// [ℹ] return to HOMEPAGE (/ <en>)
+		return {
+			status: 302,
+			redirect: '/'
+		};
+
+	}
+</script>
