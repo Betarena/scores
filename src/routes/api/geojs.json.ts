579c4747475fa247910aa3c4fc793a33eb77f55e
migbash <20924663+migbash@users.noreply.github.com>
2022-06-12 02:52:50 +0100

    testing GEO-POS determination from load() + myserver.js + hooks.ts + cookie-set v8

--- src/routes/api/geojs.json.ts
+++ src/routes/api/geojs.json.ts
@@ -1,7 +1,7 @@
 /** 
  * @type {import('@sveltejs/kit').RequestHandler} 
 */
-export async function get(req: any res: any): Promise< unknown > {
+export async function get(req: any, res: any): Promise< unknown > {
 
   const ip = req.headers.get("x-forwarded-for") || req.socket.remoteAddress || req.headers.get("x-real-ip") || null;
 
