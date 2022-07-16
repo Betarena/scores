f11770310edbdedbf32e68bf00e210a092dcb0da
MigBash <20924663+migbash@users.noreply.github.com>
2022-07-16 21:26:02 +0100

    Feature/tournaments top players (#492)
    * adding support for 	op_players widget
    
    * creating the `top_players` widget frontend
    
    * adding `models` `graphql` `caching` update + widget endpoints
    
    * `.gitignore` update + `country` redirect homepage
    
    * Updating `top_players` dark-mode
    
    * Updating widget `fixes` UI
    
    * Renaming assets
    
    * Updating `workspace`
    
    * Updating `sleep` for `placeholder`
    
    * Adding `no-data` translations to `top-players` widget
    
    * Updatign fixes to wdiget `data-display`
    
    * Adding `fixed` ellipsis to `player-name` top-player widget
    
    * Adding `fixed` ellipsis to `player-name` top-player widget v2
    
    * Adding `fixed` ellipsis to `player-name` top-player widget v3
    
    * Adding `fixed` ellipsis to `player-name` top-player widget v4
    
    * Adding `fixed` ellipsis to `player-name` top-player widget v5
    
    * Updating `caching` season re-ordering

---
+++ src/lib/graphql/tournaments/top_players/query.ts
@@ -0,0 +1,97 @@
+// [ℹ] import necessary libraries;
+import { gql } from 'graphql-request';
+
+/**
+ * [ℹ] TOURNAMENTS / TOP PLAYER WIDGET
+ * [ℹ] WARNING [⚠]
+ * [ℹ] LARGE DATA HANDLING (Query)
+ * [ℹ] REQUIRES PAGINATION APPROACH
+*/
+export const REDIS_CACHE_PREP_GET_TOURNAMENTS_TOP_PLAYERS_DYNAMIC_DATA = gql`
+  query REDIS_CACHE_PREP_GET_TOURNAMENTS_TOP_PLAYERS_DYNAMIC_DATA 
+    ($limit: Int, $offset: Int) 
+    @cached 
+    (ttl: 300)
+  {
+
+    # [ℹ] pagination based
+
+    scores_football_seasons_details_dev_aggregate {
+      aggregate {
+        totalCount: count
+      }
+    }
+
+    scores_football_seasons_details_dev (
+      limit: $limit 
+      offset: $offset
+    ) {
+      id
+      league_id
+      goalscorers
+      assistscorers
+      squad
+    }
+  }
+`
+
+/**
+ * [ℹ] TOURNAMENTS / TOP PLAYER WIDGET
+ * [ℹ] WARNING [⚠]
+ * [ℹ] LARGE DATA HANDLING (Query)
+ * [ℹ] REQUIRES PAGINATION APPROACH
+*/
+export const REDIS_CACHE_PREP_GET_TOURNAMENTS_TOP_PLAYERS_CONST_DATA = gql`
+  query REDIS_CACHE_PREP_GET_TOURNAMENTS_TOP_PLAYERS_CONST_DATA 
+    @cached 
+    (ttl: 300) 
+  {
+
+    # [ℹ] unecessary to paginate
+    
+    scores_football_leagues_dev {
+      country
+      data
+      name
+      id
+      season
+      seasons
+    }
+    scores_football_teams_dev {
+      data
+      id
+      name
+    }
+    scores_football_players_dev {
+      data
+      player_id
+      common_name
+      nationality
+    }
+  }
+`;
+
+/**
+ * [ℹ] TOURNAMENTS / TOP PLAYER WIDGET [TRANSLATION]
+*/
+export const HASURA_BETARENA_QUERY_TOP_PLAYERS_T = gql`
+  query HASURA_BETARENA_QUERY_TOP_PLAYERS_T 
+    @cached 
+    (ttl: 300) 
+  {
+    # [ℹ] unecessary to paginate
+    scores_widget_top_players_translations_dev { 
+      lang
+      data
+    }
+    player_positions_translations_dev {
+      lang
+      position
+    }
+    scores_general_translations_dev {
+      lang
+      widgets_no_data_available
+    }
+
+  }
+`;
