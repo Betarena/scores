7b58a95c8035ed4c405ab3604c8a9c6ac74a9313
migbash <20924663+migbash@users.noreply.github.com>
2022-08-11 20:32:35 +0100

    Updating `cache` for `BULL` check endpoint

--- src/routes/api/cache-bull-status.ts
+++ src/routes/api/cache-bull-status.ts
@@ -1,10 +1,17 @@
-// [ℹ] import $app `modules`
 import { dev } from '$app/env'
 
-// [❗] critical
 import Bull from 'bull';
-const cacheQueueTourTopPlay = new Bull('cacheQueueTourTopPlay', 
-  { 
+
+/** 
+ * @type {import('@sveltejs/kit').RequestHandler} 
+*/
+export async function get (req: { url: { [x: string]: { get: (arg0: string) => string; }; }; }, res: any): Promise < unknown > {
+
+  const jobQueueName: string = req.url['searchParams'].get('jobQueueName');
+  const jobId: string = req.url['searchParams'].get('jobId');
+
+  const cacheQueueJob = new Bull(jobQueueName, 
+  {
     redis: { 
       port: import.meta.env.VITE_REDIS_BULL_ENDPOINT.toString(), 
       host: import.meta.env.VITE_REDIS_BULL_HOST.toString(), 
@@ -13,23 +20,21 @@ const cacheQueueTourTopPlay = new Bull('cacheQueueTourTopPlay',
     }
   });
 
-/** 
- * @type {import('@sveltejs/kit').RequestHandler} 
-*/
-export async function get (req, res): Promise < any > {
-
-  const jobId: string = req.url['searchParams'].get('jobId');
-  const jobW = await cacheQueueTourTopPlay.getJob(jobId);
+  const jobW = await cacheQueueJob.getJob(jobId);
 
   if (jobW === null) {
+    // [ℹ] ALT
     // res.status(404).end();
+    // [ℹ] ALT 2
     return {
       status: 404,
       body: {
         msg: null
       }
     }
-  } else {
+  } else {    
+    // [ℹ] ALT
+    // res.json({ jobId, state, progress, reason });
     const state = await jobW.getState();
     const progress = jobW.progress;
     const attemptsMade = jobW.attemptsMade;
@@ -37,7 +42,7 @@ export async function get (req, res): Promise < any > {
     const finishedOn = jobW.finishedOn;
     const reason = jobW.failedReason;
     const result = jobW.returnvalue;
-    // res.json({ jobId, state, progress, reason });
+    // [ℹ] ALT 2
     return {
         status: 200,
         body: { 
