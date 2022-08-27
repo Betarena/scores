3b5d3864e681cff156310c73369c4ea6864c4b75
migbash <20924663+migbash@users.noreply.github.com>
2022-08-28 00:58:35 +0100

    Updating `round` & `weeks` data types

--- src/lib/models/tournaments/fixtures_odds/types.ts
+++ src/lib/models/tournaments/fixtures_odds/types.ts
@@ -6,6 +6,9 @@ import type {
   BETARENA_HASURA_scores_widget_football_fixtures_odds_translations, 
   BETARENA_HASURA_sportsbook_details,
   FixturesOddsTranslations,
+  Weekdays,
+  WelcomeMonths,
+  WidgetsNoDataAvailable,
 } from "$lib/models/hasura"
 
 /**
@@ -36,39 +39,53 @@ export interface REDIS_CACHE_SINGLE_tournaments_fixtures_odds_widget_data_respon
   seasons?:    Tournament_Season_Fixtures_Odds[]
 }
 
-export interface REDIS_CACHE_SINGLE_tournaments_fixtures_odds_widget_t_data_response extends FixturesOddsTranslations {
+export interface REDIS_CACHE_SINGLE_tournaments_fixtures_odds_widget_t_data_response extends FixturesOddsTranslations, WelcomeMonths, Weekdays, WidgetsNoDataAvailable {
   lang: string
 }
 
 export interface Tournament_Season_Fixtures_Odds {
   season_id?: number
+  rounds?:    Rounds_Data[]
+  weeks?:     Weeks_Data[]
   fixtures?:  Tournament_Fixture_Odds[]
 }
 
-export interface Tournament_Fixture_Odds {
-  round: number         // Rounds and weeks = scores_football_seasons_details + round_data + scores_general_translations
-  week: number          // Rounds and weeks = scores_football_seasons_details + round_data + scores_general_translations
-  // date: string          // Day and Month = historic_fixtures + "league_id" + "time" +  scores_general_translations (Filter fixtures between dates or rounds to get fixtures day information, varies depending on the computer date and time of the end user) 
-                        // [❓] whats the: historic_fixtures + "league_id" FOR ?
-  
-  fixture_date: string  // Fixture Date = historic_fixtures + "time" (Varies depending on the computer date and time of the end user)
+export interface Rounds_Data {
+  name?:   string
+  s_date?: string
+  e_date?: string
+}
 
-  live_minute: number   // live option, from firebase (non-cache-based)  // Fixture Live Time Information (Status LIVE) = livescore_now  + ""minute": 13,"
-                        // [❓] should be real-time [?] as in, without the user refreshing the app will autamtically show the fixture data is LIVE
+export interface Weeks_Data {
+  name:   string
+  s_date: string
+  e_date: string
+}
+
+export interface Tournament_Fixture_Odds {
+  round: number             // Rounds and weeks = scores_football_seasons_details + round_data + scores_general_translations
+  week: number              // Rounds and weeks = scores_football_seasons_details + round_data + scores_general_translations
+  // date: string             // Day and Month = historic_fixtures + "league_id" + "time" +  scores_general_translations (Filter fixtures between dates or rounds to get fixtures day information, varies depending on the computer date and time of the end user) 
+                            // [❓] whats the: historic_fixtures + "league_id" FOR ?
+  
+  fixture_date: string      // Fixture Date = historic_fixtures + "time" (Varies depending on the computer date and time of the end user)
+
+  live_minute: number       // live option, from firebase (non-cache-based)  // Fixture Live Time Information (Status LIVE) = livescore_now  + ""minute": 13,"
+                            // [❓] should be real-time [?] as in, without the user refreshing the app will autamtically show the fixture data is LIVE
   teams: {
     home: Fixture_Odds_Team
     away: Fixture_Odds_Team
   }
 
-  tip_link: string      // Tip = tip_link_wp + scores_general_translations
-  fixture_link: string  // Fixture Link = fixture_link_wp
-  media_link: string    // media_link = historic_fixtures + media_link + "video_link"
-  bet_icon?: string     // Betting Site Icon = sportsbook_details (GEO or forced header option)
-                        // [❔] only 1 betting site at the moment for all fixtures of X GEO (design is wrong)
+  tip_link: string          // Tip = tip_link_wp + scores_general_translations
+  fixture_link: string      // Fixture Link = fixture_link_wp
+  media_link: string        // media_link = historic_fixtures + media_link + "video_link"
+  bet_icon?: string         // Betting Site Icon = sportsbook_details (GEO or forced header option)
+                            // [❔] only 1 betting site at the moment for all fixtures of X GEO (design is wrong)
 }
 
 export interface Fixture_Odds_Team {
   name:      string   // Team Names = historic_fixtures + "home_team_name" + "away_team_name"
-  score:     string   // Score = historic_fixtures + "ft_score" [❓] what is this field [complex property]
-  red_cards: string   // Red Cards = historic_fixtures + "cards"
+  score:     number   // Score = historic_fixtures + "ft_score" [❓] what is this field [complex property]
+  red_cards: number   // Red Cards = historic_fixtures + "cards"
 }
