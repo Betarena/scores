d6968b609de347a22ea5f8ca4c5bc298bbe1b719
migbash <20924663+migbash@users.noreply.github.com>
2022-10-05 18:32:40 +0100

    update `hasura-endpoint` sitemap connection

--- src/lib/graphql/pages_and_seo/query.ts
+++ src/lib/graphql/pages_and_seo/query.ts
@@ -1,5 +1,9 @@
 import { gql } from 'graphql-request';
 
+/**
+ * [ℹ] Complete Page & Seo Query
+ * [ℹ] Caching & Sitemap generation 
+*/
 export const REDIS_CACHE_PAGES_AND_SEO = gql`
   query REDIS_CACHE_PAGES_AND_SEO 
   @cached
@@ -57,9 +61,68 @@ export const REDIS_CACHE_PAGES_AND_SEO = gql`
     historic_fixtures
     (
       # limit: 50,
+      # FIXME: temporary "where"
+      # FIXME: remove in PROD
       where: {
         id: {
           _eq: 18535246
+        }
+      }
+    ) {
+      id
+      urls
+      publish_status
+      away_team_name
+      home_team_name
+      league_name
+      fixture_day
+      league_id
+      venue_name_j: data(path: "$.venue.data.name")
+      venue_city_j: data(path: "$.venue.data.city")
+      country_id_j: data(path: "$.league.data.country_id")
+    }
+  }
+`;
+
+/**
+ * [ℹ] Surgical Target Fixture
+ * [ℹ] Caching & Sitemap Update 
+*/
+export const REDIS_CACHE_PAGES_AND_SEO_FIXTURE_TARGET = gql`
+  query REDIS_CACHE_PAGES_AND_SEO_FIXTURE_TARGET 
+    (
+      $fixtureId: Int!
+    ) 
+    @cached 
+    (ttl: 300)
+  {
+    scores_hreflang {
+      hreflang
+      link
+    }
+    scores_seo_fixtures {
+      lang
+      main_data
+      opengraph
+      sports_type
+      twitter_card
+    }
+    scores_endpoints_translations {
+      countries_translation
+      lang
+      sport
+      sports_translation
+    }
+    scores_football_countries {
+      id
+      name
+    }
+    historic_fixtures
+    (
+      # limit: 50,
+      where: {
+        id: {
+          _eq: $fixtureId
         }
       }
     ) {
