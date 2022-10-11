4fac72e04b7dab9e0f17aca8081a942e6e5c0257
migbash <20924663+migbash@users.noreply.github.com>
2022-10-11 10:39:21 +0100

    add `cache` - scoreboard retrieval

---
+++ src/routes/api/cache/fixtures/scoreboard/+server.ts
@@ -0,0 +1,46 @@
+// [ℹ] import $app `modules`
+import { dev } from '$app/environment'
+import redis from "$lib/redis/init"
+import { error, json } from '@sveltejs/kit';
+
+import type { 
+  REDIS_CACHE_SINGLE_scoreboard_data
+} from '$lib/models/tournaments/top_players/types';
+
+const cache_data_addr = "scoreboard_data"
+
+/** 
+ * @type {import('@sveltejs/kit').RequestHandler} 
+*/
+export async function GET (req, res): Promise < unknown > {
+
+  const lang: string = req.url['searchParams'].get('lang');
+  const fixture_id: string = req.url['searchParams'].get('fixture_id');
+
+  if (fixture_id) {
+    const response_cache = await get_cache_main_data (fixture_id)
+    if (response_cache) {
+      return json(response_cache)
+    }
+  }
+
+  return json(null)
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~
+//     CACHING w/ REDIS
+// ~~~~~~~~~~~~~~~~~~~~~~~~
+
+async function get_cache_main_data (league_id: string): Promise < REDIS_CACHE_SINGLE_scoreboard_data | Record < string, never > > {
+  try {
+    const cached: string = await redis.hget(cache_data_addr, league_id);
+    if (cached) {
+      const parsed: REDIS_CACHE_SINGLE_scoreboard_data = JSON.parse(cached);
+      return parsed;
+    }
+  } 
+  catch (e) {
+    console.error(`❌ uh-oh! ${cache_data_addr} cache error`, e);
+    return
+  }
+}
