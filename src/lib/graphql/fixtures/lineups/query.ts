649458527e2d4df9767417905535e1c93f444a1b
migbash <20924663+migbash@users.noreply.github.com>
2022-10-23 03:54:27 +0100

    add `status_j` surgical for `lineups`

--- src/lib/graphql/fixtures/lineups/query.ts
+++ src/lib/graphql/fixtures/lineups/query.ts
@@ -90,6 +90,7 @@ export const REDIS_CACHE_LINEUPS_DATA_1 = gql`
       events_j: data(path: "$.events.data")
       localteam_short_code_j: data(path: "$.localTeam.data.short_code")
       visitorteam_short_code_j: data(path: "$.visitorTeam.data.short_code")
+      status_j: data(path: "$.time.status")
     }
   }
 `;
