339dbbf1cddf1204c6f61eabea7412b963f16ebd
migbash <20924663+migbash@users.noreply.github.com>
2022-11-21 17:13:18 +0000

    update `about` endpoint `retrival`

--- src/routes/api/cache/fixtures/about/+server.ts
+++ src/routes/api/cache/fixtures/about/+server.ts
@@ -1,13 +1,9 @@
 import { dev } from '$app/environment'
-import redis from "$lib/redis/init"
 import { error, json } from '@sveltejs/kit';
 
-import type { 
-  REDIS_CACHE_SINGLE_about_data, 
-  REDIS_CACHE_SINGLE_about_translation 
-} from '$lib/models/fixtures/about/types';
+import redis from "$lib/redis/init"
 
-const cache_data_addr = "fixture_about_data"
+const cache_data_addr  = "fixture_about_data"
 const cache_trans_addr = "fixture_about_trans"
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
@@ -17,7 +13,11 @@ export async function GET (req, res): Promise < unknown > {
   const fixture_id: string = req.url['searchParams'].get('fixture_id');
 
   if (lang && !fixture_id) {
-    const response_cache = await get_cache_translation_data (lang)
+    const response_cache = 
+      await get_target_hset_cache_data (
+        cache_trans_addr,
+        lang
+    );
     if (response_cache) {
       return json(response_cache)
     }
@@ -25,7 +25,11 @@ export async function GET (req, res): Promise < unknown > {
 
   if (lang && fixture_id) {
     const id = fixture_id + "_" + lang
-    const response_cache = await get_cache_main_data (id)
+    const response_cache = 
+      await get_target_hset_cache_data (
+        cache_data_addr,
+        id
+    );
     if (dev) console.log(`id: ${id}`)
     if (response_cache) {
       return json(response_cache)
@@ -35,38 +39,23 @@ export async function GET (req, res): Promise < unknown > {
   return json(null)
 }
 
-// ~~~~~~~~~
-// REDIS
-// ~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~
+//  [MAIN] CACHE [GET] METHODS
+// ~~~~~~~~~~~~~~~~~~~~~~~~
 
-async function get_cache_main_data (
-  fixture_id: string
-): Promise < REDIS_CACHE_SINGLE_about_data | Record < string, never > > {
+async function get_target_hset_cache_data (
+  key: string,
+  id:  string
+): Promise < unknown > {
   try {
-    const cached: string = await redis.hget(cache_data_addr, fixture_id);
+    const cached: string = await redis.hget(key, id);
     if (cached) {
-      const parsed: REDIS_CACHE_SINGLE_about_data = JSON.parse(cached);
+      const parsed: unknown = JSON.parse(cached);
       return parsed;
     }
   } 
   catch (e) {
-    console.error(`❌ uh-oh! ${cache_data_addr} cache error`, e);
-    return
-  }
-}
-
-async function get_cache_translation_data (
-  lang: string
-): Promise < REDIS_CACHE_SINGLE_about_translation | Record < string, never > > {
-  try {
-    const cached: string = await redis.hget(cache_trans_addr, lang);
-    if (cached) {
-      const parsed: REDIS_CACHE_SINGLE_about_translation = JSON.parse(cached);
-      return parsed;
-    }
-  } 
-  catch (e) {
-    console.error(`❌ uh-oh! ${cache_trans_addr} cache error`, e);
+    console.error(`❌ uh-oh! ${key} cache error`, e);
     return
   }
 }
