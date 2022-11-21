5485dcdec29e5386e168a7cfc250213801fc86a4
migbash <20924663+migbash@users.noreply.github.com>
2022-11-21 17:13:01 +0000

    update `pages_and_seo` data cache `retireval` endpoint

--- src/routes/api/cache/_main_/pages_and_seo/+server.ts
+++ src/routes/api/cache/_main_/pages_and_seo/+server.ts
@@ -3,21 +3,34 @@ import { error, json } from '@sveltejs/kit';
 
 import redis from "$lib/redis/init"
 
-const cache_sitemap_urls_key = "sitemap"
-const cache_sitemap_v2_urls_key = "sitemap_v2"
-const cache_homepage_seo_key = "homepage_seo"
-const cache_tournaments_seo_key = "tournaments_seo"
-const cache_tournaments_info_key = "tournaments_page_info"
-const cache_fixtures_seo_key = "fixtures_seo"
-const cache_fixtures_info_key = "fixtures_page_info"
+const cache_sitemap_urls_key      = "sitemap"
+const cache_homepage_seo_key      = "homepage_seo"
+const cache_tournaments_seo_key   = "tournaments_seo"
+const cache_tournaments_info_key  = "tournaments_page_info"
+const cache_fixtures_seo_key      = "fixtures_seo"
+const cache_fixtures_info_key     = "fixtures_page_info"
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET (req, res): Promise < any > {
 
-  const url: string = req.url['searchParams'].get('url');
+  const url:  string = req.url['searchParams'].get('url');
   const lang: string = req.url['searchParams'].get('lang');
   const page: 'homepage' | 'tournaments' | 'fixtures' = req.url['searchParams'].get('page');
 
+  // [sitemap-check V2]
+  if (url && !page && !lang) {
+    const response_cache = 
+      await get_target_set_cache_data (
+        cache_sitemap_urls_key,
+        url
+    );
+    if (response_cache == 1) {
+      return json(response_cache)
+    }
+    return json(null);
+  }
+
+  // [home-page-SEO-GET]
   if (lang && page === 'homepage') {
     const response_cache = 
       await get_target_hset_cache_data (
@@ -29,6 +42,7 @@ export async function GET (req, res): Promise < any > {
     }
   }
 
+  // [tournament-page-INFO-GET]
   if (url && page === 'tournaments') {
     const response_cache = 
       await get_target_hset_cache_data (
@@ -40,17 +54,7 @@ export async function GET (req, res): Promise < any > {
     }
   }
 
-  if (url && !page && !lang) {
-    const response_cache = 
-      await get_target_hset_cache_data (
-        cache_sitemap_urls_key,
-        url
-    );
-    if (response_cache) {
-      return json(response_cache)
-    }
-  }
-
+  // [tournament-page-SEO-GET]
   if (lang && page === 'tournaments') {
     const response_cache = 
       await get_target_hset_cache_data (
@@ -62,17 +66,19 @@ export async function GET (req, res): Promise < any > {
     }
   }
 
+  // [fixture-page-INFO-GET]
   if (url && page === 'fixtures') {
     const response_cache = 
       await get_target_hset_cache_data (
         cache_fixtures_info_key,
-        lang
+        url
     );
     if (response_cache) {
       return json(response_cache)
     }
   }
 
+  // [fixture-page-SEO-GET]
   if (lang && page === 'fixtures') {
     const response_cache = 
       await get_target_hset_cache_data (
@@ -103,7 +109,22 @@ async function get_target_hset_cache_data (
     }
   } 
   catch (e) {
-    console.error("❌ uh-oh! homepage_seo cache error", e);
+    console.error(`❌ uh-oh! ${key} cache error`, e);
+    return
+  }
+}
+
+async function get_target_set_cache_data (
+  key: string,
+  id:  string
+): Promise < unknown > {
+  try {
+    const cached: number = await redis.sismember(key, id);
+    console.log(id, cached)
+    return cached;
+  } 
+  catch (e) {
+    console.error(`❌ uh-oh! ${key} cache error`, e);
     return
   }
 }
