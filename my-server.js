f72104400bbbbeff99df81e01c2c7ce60ac0d112
migbash <20924663+migbash@users.noreply.github.com>
2023-01-17 15:37:29 +0000

    `add` for `https` deploy

--- my-server.js
+++ my-server.js
@@ -1,19 +1,22 @@
 // [ℹ] https://github.com/sveltejs/kit/tree/master/packages/adapter-node#custom-server
 // [ℹ] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
 import express from 'express';
+import http from 'http';
+import https from 'https';
 
 import { handler } from './build/handler.js';
 // import sslRedirect from 'heroku-ssl-redirect';
 import compression from 'compression'; // https://expressjs.com/en/resources/middleware/compression.html
+import * as sslify from 'express-sslify';
 import * as requestIp from 'request-ip'; // https://www.npmjs.com/package/request-ip
 
-// import fs from 'fs';
+import fs from 'fs';
 
-// var options = {
-//     key: fs.readFileSync('./certs/privkey.pem'),
-//     cert: fs.readFileSync('./certs/cert.pem'),
-//   }
-// ;
+var options = {
+    key: fs.readFileSync('./certs/privkey.pem'),
+    cert: fs.readFileSync('./certs/cert.pem'),
+  }
+;
 
 const app = express();
 
@@ -72,18 +75,18 @@ app.get('/getClientIP', (req, res) => {
  * [ℹ] https://jaketrent.com/post/https-redirect-node-heroku
  * [ℹ] https://webdva.github.io/how-to-force-express-https-tutorial 
 */
-// app.use((req, res, next) => {
-//   if (req.header('x-forwarded-proto') !== 'https')
-//     res.redirect(`https://${req.header('host')}${req.url}`);
-//   else
-//     next();
-// });
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https')
+    res.redirect(`https://${req.header('host')}${req.url}`);
+  else
+    next();
+});
 
 /**
  * [ℹ] https://www.npmjs.com/package/express-sslify
  * [ℹ] https://stackoverflow.com/questions/51234023/heroku-nodejs-redirect-http-to-http
 */
-// app.use(sslify.HTTPS({ trustProtoHeader: true }))
+app.use(sslify.HTTPS({ trustProtoHeader: true }))
 
 // [ℹ] compress all responses
 app.use(compression())
@@ -98,18 +101,18 @@ app.use(handler);
  * [ℹ] initialize app;
  * [ℹ] https://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-o 
 */
-app.listen(process.env.PORT, () => {
-	console.log(`listening on port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+// 	console.log(`listening on port ${process.env.PORT}`);
+// });
 
 // ~~~~~~~~~~~~~~~~~~~~~~
 // APP SPIN-UP [PROD] [V2]
 // ~~~~~~~~~~~~~~~~~~~~~~
 
-// http.createServer(app).listen(process.env.PORT, function(){
-//   console.log(`[HTTP | Server]: Server is running on port: ${process.env.PORT}`);
-// });
+http.createServer(app).listen(process.env.PORT, function(){
+  console.log(`[HTTP | Server]: Server is running on port: ${process.env.PORT}`);
+});
 
-// https.createServer(options, app).listen(443, function(){
-//   console.log(`[HTTPS | Server]: Server is running on port: ${443}`);
-// });
+https.createServer(options, app).listen(443, function(){
+  console.log(`[HTTPS | Server]: Server is running on port: ${443}`);
+});
