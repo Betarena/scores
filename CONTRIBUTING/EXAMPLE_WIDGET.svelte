6d754bc498565443751f68d10c8555456bf70a0f
migbash <20924663+migbash@users.noreply.github.com>
2023-01-25 10:35:52 +0000

    `update` example widget

---
+++ CONTRIBUTING/EXAMPLE_WIDGET.svelte
@@ -0,0 +1,38 @@
+<!-- ===============
+COMPONENT JS (w/ TS)
+=================-->
+
+<script lang="ts">
+
+  // ~~~~~~~~~~~~~~~~~~~~~
+  //  COMPONENT VARIABLES
+  // ~~~~~~~~~~~~~~~~~~~~~
+
+  export const EXAMPLE_VALUE
+  
+  const EXAMPLE_VALUE
+
+  // ~~~~~~~~~~~~~~~~~~~~~
+  //  COMPONENT METHODS
+  // ~~~~~~~~~~~~~~~~~~~~~
+
+  function do_something() {}
+
+  // ~~~~~~~~~~~~~~~~~~~~~
+  // VIEWPORT CHANGES
+  // ~~~~~~~~~~~~~~~~~~~~~
+
+</script>
+
+<!-- ===============
+COMPONENT HTML 
+=================-->
+
+<!-- HTML GOES HERE -->
+
+<!-- ===============
+COMPONENT STYLE
+=================-->
+
+<style>
+</style>
