ac10e2f8df3b4cdc78aa16bdd710f02621f9a031
migbash <20924663+migbash@users.noreply.github.com>
2023-02-08 07:42:56 +0000

    feat: update debug func;

--- src/lib/utils/debug.ts
+++ src/lib/utils/debug.ts
@@ -1,3 +1,9 @@
+import { dev } from "$app/environment";
+
+/**
+ * @param groupName 
+ * @param msg 
+ */
 export function logErrorGroup (
   groupName:string, 
   msg: string
@@ -8,6 +14,10 @@ export function logErrorGroup (
   console.groupEnd()
 }
 
+/**
+ * @param groupName 
+ * @param msg 
+ */
 export function logDevGroup (
   groupName:string, 
   msg: string
@@ -18,6 +28,10 @@ export function logDevGroup (
   console.groupEnd()
 }
 
+/**
+ * @param groupName 
+ * @param msgs 
+*/
 export function log_info_group (
   groupName:string, 
   msgs: string[]
@@ -28,4 +42,53 @@ export function log_info_group (
     console.log(`${msg}`)
   }
   console.groupEnd()
+}
+
+/**
+ * @param msg 
+*/
+export function dlog (
+  msg: string | object,
+  show?: boolean
+) {
+  // [🐞]
+  if (dev && show) console.debug(msg)
+}
+
+
+/**
+ * @param groupName 
+ * @param msgs 
+ * @param show 
+ * @param style 
+*/
+export function dlogv2 (
+  groupName:string, 
+  msgs: object[] | string[],
+  show?: boolean,
+  style?: string
+) {
+  // [🐞]
+  if (dev && show) {
+    console.groupCollapsed(`%c${groupName}`, style)
+    for (const m of msgs) {
+      const msg = 
+        typeof(m) == 'string'
+          ? m.replace(/\t/g, '')
+          : m
+      ;
+      console.debug(msg)
+    }
+    console.groupEnd()
+  } 
+}
+
+/**
+ * @param msg 
+*/
+export function errlog (
+  msg: string
+) {
+  // [🐞]
+  if (dev) console.error(`❌ Error: ${msg}`)
 }
