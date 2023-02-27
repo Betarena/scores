8ef620b77d2a2f9421040e66d73f25161cd5ea14
migbash <20924663+migbash@users.noreply.github.com>
2023-02-27 22:52:47 +0000

    feat: update loaders

---
+++ src/lib/components/page/home/livescores-v2/Livescores_Loader.svelte
@@ -0,0 +1,114 @@
+<!-- ===============
+COMPONENT JS (w/ TS)
+=================-->
+
+<script lang="ts">
+
+  //#region ➤ [MAIN] Package Imports
+  // IMPORTS GO HERE
+
+  //#region ➤ Svelte/SvelteKit Imports
+  // IMPORTS GO HERE
+  //#endregion ➤ Svelte/SvelteKit Imports
+
+  //#region ➤ Project Custom Imports
+  // IMPORTS GO HERE
+  //#endregion ➤ Project Custom Imports
+
+  //#region ➤ Firebase Imports
+  // IMPORTS GO HERE
+  //#endregion ➤ Firebase Imports
+
+  //#region ➤ Types Imports
+  // IMPORTS GO HERE
+  //#endregion ➤ Types Imports
+
+  //#region ➤ Assets Imports
+  // IMPORTS GO HERE
+  //#endregion ➤ Assets Imports
+
+	import LivescoresTopRow from './Livescores_Top_Row.svelte';
+	import LoaderRow from './loaders/Loader_Row.svelte';
+
+  //#endregion ➤ [MAIN] Package Imports
+
+  //#region ➤ [VARIABLES]
+
+  // ~~~~~~~~~~~~~~~~~~~~~
+  //  COMPONENT VARIABLES
+  // ~~~~~~~~~~~~~~~~~~~~~
+
+  //#endregion ➤ [VARIABLES]
+
+  //#region ➤ [METHODS]
+
+  // ~~~~~~~~~~~~~~~~~~~~~
+  //  COMPONENT METHODS
+  // ~~~~~~~~~~~~~~~~~~~~~
+
+  // -Methods Go Here
+
+  function do_something() {}
+
+  // ~~~~~~~~~~~~~~~~~~~~~
+  // VIEWPORT CHANGES
+  // ~~~~~~~~~~~~~~~~~~~~~
+
+  //#endregion ➤ [METHODS]
+
+  //#region ➤ [ONE-OFF] [METHODS] [IF]
+
+  //#endregion ➤ [ONE-OFF] [METHODS] [IF]
+
+  //#region ➤ [REACTIVIY] [METHODS]
+
+  //#endregion ➤ [REACTIVIY] [METHODS]
+
+  //#region ➤ SvelteJS/SvelteKit [LIFECYCLE]
+
+  //#endregion ➤ SvelteJS/SvelteKit [LIFECYCLE]
+
+  LoaderRow
+
+</script>
+
+<!-- ===============
+COMPONENT HTML 
+NOTE: [HINT] use (CTRL+SPACE) to select a (class) (id) style
+=================-->
+
+<div
+  class="widget-component">
+  <LivescoresTopRow 
+    numOfFixtures={0}
+    numOfFixturesLive={0}
+  />
+  <div>
+    {#each { length: 10 } as _, i}
+      <div
+        class="m-b-10"
+        style="
+          border-top: 1px solid var(--grey-color);
+          padding-top: 20px;
+          padding-bottom: 10px;
+          /* margin: auto; */
+          padding: 20px;
+          text-align: center;
+        ">
+        <LoaderRow />
+      </div>
+    {/each}
+  </div>
+</div>
+
+<!-- ===============
+COMPONENT STYLE
+NOTE: [HINT] auto-fill/auto-complete iniside <style> for var() values by typing/(CTRL+SPACE)
+=================-->
+
+<style>
+
+  @media only screen and (min-width: 726px) and (max-width: 1000px) {
+  }
+
+</style>
