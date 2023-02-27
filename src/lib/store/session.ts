19aae74ae46f07ace405d8a86c2a11fcbb222f88
migbash <20924663+migbash@users.noreply.github.com>
2023-02-27 15:36:02 +0000

    feat: enhance session (stores);

--- src/lib/store/session.ts
+++ src/lib/store/session.ts
@@ -1,20 +1,34 @@
+import type { FIREBASE_livescores_now } from '$lib/models/firebase';
+import type { Cache_Single_SportbookDetails_Data_Response } from '$lib/models/tournaments/league-info/types';
 import { writable } from 'svelte/store';
-
 export interface Platform_Session {
-	newsletterPopUpShow: boolean; // [ℹ] Email_subscribe.svelte
-	selectedSeasonID: number; // [ℹ] Tournament Page Critical [❗]
-	fixture_select_view: 'overview' | 'news'; // [ℹ] Fixture Page View Critical [❗]
-	auth_show: boolean; // [ℹ] Authenticated Opt
-  lang_intent: string | undefined;  // NOTE: used for detecting and pre-loading the data for a TARGET page translation of the current one, programatically
+  // [ℹ] used by email (footer) pop-up modal
+	newsletterPopUpShow: boolean;
+  // NOTE:IMPORTANT - used for inter-component events of selected season changed (reactivity)
+	selectedSeasonID: number;
+  // NOTE:IMPORTANT - used for inter-component events of selected view on fixtures page
+	fixture_select_view: 'overview' | 'news';
+  // [ℹ] used to show/hide auth pop-up modal
+	auth_show: boolean;
+  // NOTE: used for detecting and pre-loading the data for a TARGET page translation of the current one, programatically
+  lang_intent: string | undefined;
+  livescore_now: Map<number, FIREBASE_livescores_now>
+  sportbook_main: Cache_Single_SportbookDetails_Data_Response
+  sportbook_list: Cache_Single_SportbookDetails_Data_Response[]
+  livescoreNowSelectedDate: Date
 }
 
-// [ℹ] Default State
+// [ℹ] DEFAULT STORE STATE
 const seassion_store: Platform_Session = {
 	newsletterPopUpShow: false,
 	selectedSeasonID: undefined,
 	fixture_select_view: 'overview',
 	auth_show: false,
-  lang_intent: undefined
+  lang_intent: undefined,
+  livescore_now: undefined,
+  sportbook_main: undefined,
+  sportbook_list: undefined,
+  livescoreNowSelectedDate: undefined,
 };
 
 function createLocalStore() {
@@ -30,7 +44,12 @@ function createLocalStore() {
 		toggleNewsletter: () => {
 			seassion_store.newsletterPopUpShow =
 				!seassion_store.newsletterPopUpShow;
-		}
+		},
+
+    updateLivescores: (data: Map<number, FIREBASE_livescores_now>) => {
+      seassion_store.livescore_now = data
+      set(seassion_store)
+    }
 	};
 }
 
