4cfa6e943da2ddd5b067279e4f4e838e6086258d
migbash <20924663+migbash@users.noreply.github.com>
2023-02-27 15:37:15 +0000

    feat: decouple firebase (common) func (livescore);

---
+++ src/lib/firebase/common.ts
@@ -0,0 +1,56 @@
+import type { FIREBASE_livescores_now } from "$lib/models/firebase";
+import { sessionStore } from "$lib/store/session";
+import { dlog, FIREBASE_DEBUG_STYLE, FIREBASE_DEBUG_TAG, FIREBASE_DEBUG_TOGGLE } from "$lib/utils/debug";
+import { onValue, ref, type Unsubscribe } from "firebase/database";
+import { db_real } from "./init";
+
+/**
+ * @description common method that will listen to real-time
+ * changes in Livescores_Now Firebase (REAL-DB);
+ * @returns {Promise < Unsubscribe >} Promise < Unsubscribe >
+ */
+export async function listenRealTimeLivescoresNowChange(
+): Promise < Unsubscribe > {
+
+  dlog(`${FIREBASE_DEBUG_TAG} listenRealTimeLivescoresNowChange()`, FIREBASE_DEBUG_TOGGLE, FIREBASE_DEBUG_STYLE);
+  const fixtureRef = ref(
+    db_real,
+    'livescores_now/'
+  );
+  const listenEventRef = onValue(fixtureRef, (snapshot) => {
+    // [ℹ] break-down-values
+    if (snapshot.val() != null) {
+      const data: [
+        string,
+        FIREBASE_livescores_now
+      ][] = Object.entries(snapshot.val());
+      genLiveFixMap(data);
+    }
+  });
+  return listenEventRef
+}
+
+/**
+ * @description checks onValue changes for new Livescores_Now Table
+ * data changes;
+ * @param {[string, FIREBASE_livescores_now][]} data
+ * @returns NaN
+ */
+async function genLiveFixMap (
+  data: [string, FIREBASE_livescores_now][]
+): Promise < void > {
+  dlog(`${FIREBASE_DEBUG_TAG} genLiveFixMap()`, FIREBASE_DEBUG_TOGGLE, FIREBASE_DEBUG_STYLE);
+  const liveFixturesMap = new Map<number, FIREBASE_livescores_now>();
+  // [ℹ] generate live-fixtures map
+  for (const live_fixture of data) {
+    const fixture_id = parseInt(
+      live_fixture[0].toString()
+    );
+    const fixture_data = live_fixture[1];
+    liveFixturesMap.set(
+      fixture_id,
+      fixture_data
+    );
+  }
+  sessionStore.updateLivescores(liveFixturesMap)
+}
