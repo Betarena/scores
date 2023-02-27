c0e2cf683a59ff7fb1e80988caaa5a19d14f7f66
migbash <20924663+migbash@users.noreply.github.com>
2023-02-27 15:32:51 +0000

    feat: add Widget-Title (widget);

---
+++ src/lib/components/Widget-Title.svelte
@@ -0,0 +1,111 @@
+<!-- 
+====================
+This is an example .svelte
+component file, to give guidance on
+the structure that is employed across the project
+and how it should be layed-out.
+====================
+<COPY-THIS-FILE-INTO-YOUR-NEXT-COMPONENT>
+====================
+-->
+
+<!-- ===============
+COMPONENT JS (w/ TS)
+=================-->
+
+<script lang="ts">
+
+  //#region ➤ [MAIN] Package Imports
+  // IMPORTS GO HERE
+
+  //#region ➤ Svelte/SvelteKit Imports
+  // IMPORTS GO HERE
+  //#endregion ➤ Svelte/SvelteKit Imports
+
+  //#region ➤ Project Custom Imports
+  // IMPORTS GO HERE
+	import { userBetarenaSettings } from '$lib/store/user-settings';
+  //#endregion ➤ Project Custom Imports
+
+  //#region ➤ Firebase Imports
+  // IMPORTS GO HERE
+  //#endregion ➤ Firebase Imports
+
+  //#region ➤ Types Imports
+  // IMPORTS GO HERE
+  //#endregion ➤ Types Imports
+
+  //#region ➤ Assets Imports
+  // IMPORTS GO HERE
+  //#endregion ➤ Assets Imports
+
+  //#endregion ➤ [MAIN] Package Imports
+
+  //#region ➤ [VARIABLES]
+
+  // ~~~~~~~~~~~~~~~~~~~~~
+  //  COMPONENT VARIABLES
+  // ~~~~~~~~~~~~~~~~~~~~~
+
+  // -Variables Go Here
+
+  export let WIDGET_TITLE: string
+
+  //#endregion ➤ [VARIABLES]
+
+  //#region ➤ [METHODS]
+
+  // ~~~~~~~~~~~~~~~~~~~~~
+  //  COMPONENT METHODS
+  // ~~~~~~~~~~~~~~~~~~~~~
+
+  // -Methods Go Here
+
+  function do_something() {}
+
+  // ~~~~~~~~~~~~~~~~~~~~~
+  // VIEWPORT CHANGES
+  // ~~~~~~~~~~~~~~~~~~~~~
+
+  //#endregion ➤ [METHODS]
+
+  //#region ➤ [ONE-OFF] [METHODS] [IF]
+
+  //#endregion ➤ [ONE-OFF] [METHODS] [IF]
+
+  //#region ➤ [REACTIVIY] [METHODS]
+
+  //#endregion ➤ [REACTIVIY] [METHODS]
+
+  //#region ➤ SvelteJS/SvelteKit [LIFECYCLE]
+
+  //#endregion ➤ SvelteJS/SvelteKit [LIFECYCLE]
+
+</script>
+
+<!-- ===============
+COMPONENT HTML 
+NOTE: [HINT] use (CTRL+SPACE) to select a (class) (id) style
+=================-->
+
+<h2
+  class="
+    s-20 
+    m-b-10 
+    w-500 
+    color-black-2
+  "
+  style="margin-top: 0px;"
+  class:color-white={$userBetarenaSettings.theme == 'Dark'}
+>
+  {WIDGET_TITLE}
+</h2>
+
+<!-- ===============
+COMPONENT STYLE
+NOTE: [HINT] auto-fill/auto-complete iniside <style> for var() values by typing/(CTRL+SPACE)
+=================-->
+
+<style>
+
+</style>
