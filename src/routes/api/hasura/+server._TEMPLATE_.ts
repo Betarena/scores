63d4bf4a77b0d19f4e38934627eb4f33234e3464
migbash <20924663+migbash@users.noreply.github.com>
2023-03-03 21:47:12 +0000

    feat: add hasura/TEMPLATE.ts file;

---
+++ src/routes/api/hasura/+server._TEMPLATE_.ts
@@ -0,0 +1,48 @@
+// ===========================
+// NOTE: This is a TEMPLATE
+// ===========================
+// Use this file to guide yourself
+// into better structuring and
+// developing new methods and
+// +server.ts endpoints with SvelteKit
+// for retrival and processing
+// of target data;
+// ===========================
+
+//#region ➤ Package Imports
+
+import { initGrapQLClient } from '$lib/graphql/init_graphQL';
+
+//#endregion ➤ Package Imports
+
+//#region ➤ [VARIABLES] Imports
+
+const graphQlInstance = initGrapQLClient()
+// [ℹ] debug info
+const logs = [];
+
+//#endregion ➤ [VARIABLES] Imports
+
+//#region ➤ [METHODS]
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~
+//  [MAIN] ENDPOINT METHOD
+// ~~~~~~~~~~~~~~~~~~~~~~~~
+
+export async function GET(
+  req
+): Promise<void> {
+  // const X: string = req.url['searchParams'].get('X');
+	// const target_season_fixtures = await main(LEAGUE_ID, SEASON_ID);
+	// return json(target_season_fixtures);
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~
+//  [MAIN] METHOD
+// ~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~
+//  [HELPER] OTHER METHODS
+// ~~~~~~~~~~~~~~~~~~~~~~~~
+
+//#endregion ➤ [METHODS]
