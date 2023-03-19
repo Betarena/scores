715b40134c1fcb0dfd2e0c3b393269ea89d1357a
migbash <20924663+migbash@users.noreply.github.com>
2023-03-19 23:33:46 +0000

    feat: #1115; breadcrumb stable update (modular);

--- src/lib/components/page/player/Breadcrumb.svelte
+++ src/lib/components/page/player/Breadcrumb.svelte
@@ -1,21 +1,47 @@
-<!-- ===================
-	COMPONENT JS - BASIC 
-=================== -->
+<!-- ===============
+COMPONENT JS (w/ TS)
+=================-->
+
 <script lang="ts">
+
+  //#region ➤ [MAIN] Package Imports
+  // <-imports-go-here->
 
 	import { page } from "$app/stores";
 	import { platfrom_lang_ssr } from "$lib/utils/platform-functions";
 
-	import type { B_SAP_PP_D } from "@betarena/scores-lib/types/seo-pages";
+	import type {
+		B_SAP_D1,
+		B_SAP_PP_D
+	} from "@betarena/scores-lib/types/seo-pages";
+
+	import BreadcrumbSingle from "./Breadcrumb-Single.svelte";
+
+  //#endregion ➤ [MAIN] Package Imports
+
+  //#region ➤ [VARIABLES]
+
+  // ~~~~~~~~~~~~~~~~~~~~~
+  //  COMPONENT VARIABLES
+  // ~~~~~~~~~~~~~~~~~~~~~
 
   // IMPORTANT
   // (this) widget has access to the following PAGE data:
   // [...]
   // $page.data.PAGE_DATA: B_SAP_PP_D
+  // $page.data.B_SAP_D1: B_SAP_D1
   // FIXME: remove cosnt data = [...] and fix the types issue with $page.data[...]
 
   let data: B_SAP_PP_D = $page.data.PAGE_DATA
-  $: data = $page.data.PAGE_DATA
+  let data_0: B_SAP_D1 = $page.data.B_SAP_D1
+  let league_url_split: [
+    /** sport target link */
+    string,
+    /** country target link */
+    string,
+    /** league target link */
+    string
+  ]
 
   // ~~~~~~~~~~~~~~~~~~~~~
 	// (SSR) LANG SVELTE | IMPORTANT
@@ -26,17 +52,30 @@
 		$page?.error,
 		$page?.params?.lang
 	);
+
+  $: data = $page.data.PAGE_DATA
+  $: data_0 = $page.data.B_SAP_D1
+
   $: breadcrumb_lang_prefix = 
     server_side_language == 'en'
       ? `/`
       : `/${server_side_language}/`
   ;
+  $: country = data_0?.translations[server_side_language];
+  $: league_url_split = 
+    data?.alternate_data_2[server_side_language]
+    .split('/')
+    .filter(a => a.length != 2)
+  // $: console.log(league_url_split)
+
+  //#endregion ➤ [VARIABLES]
 
 </script>
 
-<!-- ===================
-	COMPONENT HTML
-=================== -->
+<!-- ===============
+COMPONENT HTML 
+NOTE: [HINT] use (CTRL+SPACE) to select a (class) (id) style
+=================-->
 
 <!-- 
 [ℹ] breadcrumbs component URL
@@ -51,101 +90,50 @@
   <!-- 
   [ℹ] sport
   -->
-  <a
-    href={`${breadcrumb_lang_prefix}${data?.data?.sport_typ}`}
-  >
-    <p
-      class="
-        s-14 
-        color-white 
-        m-r-10 
-        capitalize 
-        cursor-pointer 
-        no-wrap
-      "
-    >
-      {data?.data?.sport_typ}
-    </p>
-  </a>
-
-  <img
-    src="/assets/svg/tournaments/arrow-right.svg"
-    alt="Arrow Right Icon"
-    class="m-r-10"
-    width="14"
-    height="14"
+  <!-- TODO: correct transaltion -->
+  <BreadcrumbSingle 
+    href={`${breadcrumb_lang_prefix}${league_url_split[0]}`}
+    name={data?.data?.sport_typ}
   />
-
   <!-- 
   [ℹ] country 
   -->
-  <a
-    href={`${breadcrumb_lang_prefix}${data?.data?.country_id}`}
-  >
-    <p
-      class="
-        s-14 
-        color-white 
-        m-r-10 
-        capitalize 
-        cursor-pointer 
-        no-wrap
-      "
-    >
-      {data?.data?.country_id}
-    </p>
-  </a>
-
-  <img
-    src="/assets/svg/tournaments/arrow-right.svg"
-    alt="Arrow Right Icon"
-    class="m-r-10"
-    width="14"
-    height="14"
+  <BreadcrumbSingle 
+    href={`${breadcrumb_lang_prefix}${league_url_split[0]}/${league_url_split[1]}`}
+    name={country}
   />
-
   <!-- 
   [ℹ] league_name 
   -->
-  <a
-    href={`${breadcrumb_lang_prefix}${data?.data?.league_name}`}
-  >
-    <p
-      class="
-        s-14 
-        color-white
-        m-r-10 
-        capitalize 
-        cursor-pointer 
-        no-wrap
-      "
-    >
-      {data?.data?.league_name}
-    </p>
-  </a>
-
-  <img
-    src="/assets/svg/tournaments/arrow-right.svg"
-    alt="Arrow Right Icon"
-    class="m-r-10"
-    width="14"
-    height="14"
+  <BreadcrumbSingle 
+    href={`${breadcrumb_lang_prefix}${league_url_split[0]}/${league_url_split[1]}/${league_url_split[2]}`}
+    name={data?.data?.league_name}
   />
-
   <!-- 
   [ℹ] player_name && team_name 
   -->
-  <p
-    class="
-      s-14 
-      color-white 
-      m-r-10 
-      capitalize 
-      fixture-name
-    "
-  >
-    {data?.data?.player_name}
-    {data?.data?.team_name}
-    videos, transfer history and stats
-  </p>
+  <BreadcrumbSingle 
+    href={`${breadcrumb_lang_prefix}${league_url_split[0]}/${league_url_split[1]}/${league_url_split[2]}`}
+    name={`${data?.data?.player_name} ${data?.data?.team_name} videos, transfer history and stats`}
+    end={true}
+    disable={true}
+  />
 </div>
+
+<!-- ===============
+COMPONENT STYLE
+NOTE: [HINT] auto-fill/auto-complete iniside <style> for var() values by typing/(CTRL+SPACE)
+=================-->
+
+<style>
+
+  p.breadcrumb {
+    font-size: 14px;
+    color: var(--white);
+    margin-right: 10px;
+    text-transform: capitalize;
+		overflow: hidden;
+    white-space: nowrap;
+  }
+
+</style>
