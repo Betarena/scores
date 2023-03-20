8455cab8b9ebdf96d0c6b967036e33e98feaf2c4
migbash <20924663+migbash@users.noreply.github.com>
2023-03-20 14:11:34 +0000

    doc: add params/player.js comments;

--- src/params/player.js
+++ src/params/player.js
@@ -3,6 +3,7 @@ export function match(param) {
   // NOTE: similar to the fixture's route,
   // NOTE: thus using hard-coded values,
   // NOTE: to clearly identify the player's route
+  // DOC: https://discord.com/channels/457912077277855764/1023340103071965194/threads/1086245553203068998
   const regexList = [
     // EN
     /player/,
