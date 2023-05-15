78498933856a6c51218afed1a3411a2592713b67
migbash <20924663+migbash@users.noreply.github.com>
2023-05-15 16:52:13 +0100

    issue: #1303;

--- src/lib/components/page/player/team/Team-Widget.svelte
+++ src/lib/components/page/player/team/Team-Widget.svelte
@@ -17,7 +17,7 @@ COMPONENT JS (w/ TS)
 	import TeamLoader from './Team-Loader.svelte';
 	import TeamMain from './Team-Main.svelte';
   
-	import type { B_PTEAM_D } from '@betarena/scores-lib/types/player-team.js';
+	import type { B_PTEAM_D, B_PTEAM_T } from '@betarena/scores-lib/types/player-team.js';
 	import type { B_SAP_PP_D } from '@betarena/scores-lib/types/seo-pages.js';
 
   //#endregion ➤ [MAIN] Package Imports
@@ -30,11 +30,13 @@ COMPONENT JS (w/ TS)
 
   let PAGE_DATA: B_SAP_PP_D = $page.data?.PAGE_DATA
   let WIDGET_S_DATA: B_PTEAM_D = $page.data?.B_PTEAM_D
+  let WIDGET_T_DATA: B_PTEAM_T = $page.data?.B_PTEAM_T
   let WIDGET_DATA: B_PTEAM_D;
   let NO_WIDGET_DATA: boolean = true // [ℹ] default (true)
 
   $: PAGE_DATA = $page.data?.PAGE_DATA
   $: WIDGET_S_DATA = $page.data?.B_PTEAM_D
+  $: WIDGET_TITLE = WIDGET_T_DATA != undefined ? WIDGET_T_DATA?.widget_title || 'Current Team' : 'Current Team'
 
   //#endregion ➤ [VARIABLES]
 
@@ -132,8 +134,11 @@ NOTE: [HINT] use (CTRL+SPACE) to select a (class) (id) style
 
 <SeoBox>
   <h2>
-    {WIDGET_S_DATA?.data?.team_name}
+    {WIDGET_TITLE}
   </h2>
+  <p>
+    {WIDGET_S_DATA?.data?.team_name}
+  </p>
 </SeoBox>
 
 <!-- <TeamLoader /> -->
