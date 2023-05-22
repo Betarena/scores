4a9921afaed95473c82684c2b8fb69f6cde23147
migbash <20924663+migbash@users.noreply.github.com>
2023-05-22 09:46:18 +0100

    chore: update App.Locals interface;

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,7 +6,14 @@ declare global {
 		// interface Locals {}
 		// interface PageData {}
 		// interface Platform {}
-	}
+
+    interface Locals 
+    {
+			user: any;
+      betarenaUser: any
+		}
+
+  }
 }
 
 export { };
