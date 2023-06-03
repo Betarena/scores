19bacadaf33c74b51b8b80816ccce2c785303eb2
migbash <20924663+migbash@users.noreply.github.com>
2023-06-03 01:05:46 +0100

    chore: add google action-str;

--- src/lib/utils/google.ts
+++ src/lib/utils/google.ts
@@ -1,6 +1,8 @@
 export const googleActionsStr: {
   FP_SCRB_BET_SITE: string;
+  FP_H2H: string;
 } = {
-  FP_SCRB_BET_SITE: 'betting_site_logo_football_fixtures_scoreboard_fixtures'
+  FP_SCRB_BET_SITE: 'betting_site_logo_football_fixtures_scoreboard_fixtures',
+  FP_H2H: 'fixtures_football_fixtures_h2h'
 }
 
