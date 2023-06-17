518a4b8f3908fcbc4619b55076b286924a5cad9c
migbash <20924663+migbash@users.noreply.github.com>
2023-06-17 13:45:27 +0100

    chore: new translations default fallack;

---
+++ src/lib/utils/translation.ts
@@ -0,0 +1,20 @@
+
+/**
+ * @summary
+ * [HELPER]
+ * @description
+ * -> default translations in English, as a translation fallback;
+ */
+export const translationObject:
+{
+  sign_in:    string;
+  odds_type:  string;
+  bookmakers: string;
+  balance:    string;
+} =
+{
+  sign_in: 'Sign In',
+  odds_type: 'Odds Type',
+  bookmakers: 'Bookmakers',
+  balance: 'Balance'
+}
