88ea8d4a5e0f0010828382db25dc233b5655f758
migbash <20924663+migbash@users.noreply.github.com>
2023-07-20 09:09:11 +0100

    feat: #1422; add preliminary loader for tx-hist;

---
+++ src/lib/components/page/profile/Widget-Tx-Hist-Loader.svelte
@@ -0,0 +1,84 @@
+<!-- ===============
+COMPONENT JS (w/ TS)
+=================-->
+
+<script lang="ts">
+
+  // #region ➤ 📦 Package Imports
+
+	import LoaderBox_1 from './loaders/Loader-Box-1.svelte';
+	import LoaderBox_2 from './loaders/Loader-Box-2.svelte';
+	import LoaderBox_3 from './loaders/Loader-Box-3.svelte';
+
+  // #endregion ➤ 📦 Package Imports
+
+</script>
+
+<!-- ===============
+COMPONENT HTML
+NOTE: [HINT] use (CTRL+SPACE) to select a (class) (id) style
+=================-->
+
+<div
+  class="widget-component"
+>
+  <!-- <LivescoresTopRow
+    numOfFixtures={0}
+    numOfFixturesLive={0}
+  /> -->
+
+  <div>
+    {#each { length: 10 } as _, i}
+      <div
+        class=
+        "
+        row-space-out
+        "
+      >
+        <LoaderBox_1 />
+        <LoaderBox_2 />
+        <LoaderBox_3 />
+      </div>
+    {/each}
+  </div>
+
+</div>
+
+<!-- ===============
+COMPONENT STYLE
+NOTE: [HINT] auto-fill/auto-complete iniside <style> for var() values by typing/(CTRL+SPACE)
+=================-->
+
+<style>
+
+  :global(div.livescores-fixture-row-loader) {
+    border-top: 1px solid var(--grey-color);
+    padding-top: 20px;
+    padding-bottom: 10px;
+    padding: 20px;
+    text-align: center;
+  }
+
+  /*
+  =============
+  RESPONSIVNESS
+  =============
+  */
+
+  @media only screen and (min-width: 768px) {
+		.widget-component {
+			margin-top: 40px;
+		}
+	}
+
+  /*
+  =============
+  DARK-THEME
+  =============
+  */
+
+  :global(.dark-background div.livescores-fixture-row-loader) {
+    border-top: 1px solid var(--dark-theme-1-shade);
+  }
+
+</style>
---
+++ src/lib/components/page/profile/loaders/Loader-Box-1.svelte
@@ -0,0 +1,9 @@
+<svg
+  width="64"
+  height="20"
+  viewBox="0 0 64 20"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <rect width="64" height="20" rx="2" fill="#F2F2F2"/>
+</svg>
