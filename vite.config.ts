fe2c34b00acb58da4acc202a2e6d8c331be78153
migbash <20924663+migbash@users.noreply.github.com>
2023-07-26 00:07:22 +0100

    issue: #1278; cont;

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import viteCompression from 'vite-plugin-compression';
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 // import preload from "vite-plugin-preload";
+import fs from 'fs';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig
@@ -9,20 +10,29 @@ export default defineConfig
   {
     plugins:
     [
-      // ### DOC: vite-plugin-chunk-split
-      // ### NOTE: IMPORTANT
-      // ### Not-working - does not compile correctly.
+      // ◾️ DOC: vite-plugin-chunk-split
+      // ◾️ NOTE: IMPORTANT
+      // ◾️ does not work - does not compile correctly.
       // chunkSplitPlugin({ strategy: 'all-in-one' }),
-      // ### DOC: vite-plugin-compress
-      // progress(),
-      // ### DOC: vite-plugin-progress
-      // c.compress()
-      // viteCompression(),
-      // cssInjectedByJsPlugin
-      // (
-      //   {
 
-      //     relativeCSSInjection: true
+      // ◾️ DOC: vite-plugin-progress
+      // progress(),
+
+      // ◾️ DOC: vite-plugin-compress
+      // c.compress()
+
+
+      // preload(),
+
+      sveltekit(),
+
+      viteCompression(),
+
+      cssInjectedByJsPlugin
+      (
+        {
+
+          // relativeCSSInjection: true
 
           // topExecutionPriority: true,
 
@@ -31,27 +41,57 @@ export default defineConfig
           //   outputChunk
           // )
           // {
+
+          //   // [🐞]
+          //   // ### NOTE:
+          //   // ### It appears, the 'outputChunk.filename' is of type:
+          //   // ### - _app/immutable/chunks/index.088b98a6.js
+          //   // ### - _app/immutable/chunks/index.8e8ca4ce.js
+          //   // ### etc.
+          //   // console.log(outputChunk.fileName);
+
           //   return outputChunk.fileName == 'index.js';
           // }
 
-          // injectCode: (cssCode, options) => {
-          //   return `try{if(typeof document != 'undefined'){var elementStyle = document.createElement('style');elementStyle.appendChild(document.createTextNode(${cssCode}));document.head.appendChild(elementStyle);}}catch(e){console.error('vite-plugin-css-injected-by-js', e);}`
-          // }
-      //   }
-      // ),
-      // preload(),
-      sveltekit()
+          // ### NOTE:
+          // ### definitive 'hack' solution for 'single CSS file' output chunk.
+          injectCode:
+          (
+            cssCode,
+            options
+          ) =>
+          {
+
+            const cssCodeMod = cssCode.slice(1, -1);
+
+            fs.writeFile
+            (
+              './static/all-css-chunk.css',
+              cssCodeMod,
+              err =>
+              {
+                if (err) console.error(err);
+              }
+            );
+
+            return '';
+
+            // return `try{if(typeof document != 'undefined'){var elementStyle = document.createElement('style');elementStyle.appendChild(document.createTextNode(${cssCode}));document.head.appendChild(elementStyle);}}catch(e){console.error('vite-plugin-css-injected-by-js', e);}`
+          }
+
+        }
+      ),
     ],
     // DOC: REF: [6]
-    // build: {
-    //   rollupOptions:
-    //   {
-    //     output:
-    //     {
-    //       manualChunks: undefined
-    //     }
-    //   }
-    // },
+    build: {
+      rollupOptions:
+      {
+        output:
+        {
+          manualChunks: undefined
+        }
+      }
+    },
     // test:
     // {
     //   include: ['src/**/*.{test,spec}.{js,ts}'],
