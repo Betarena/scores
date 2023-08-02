039d0c7c4d9c9b0148b7063ab19c93031ee80634
migbash <20924663+migbash@users.noreply.github.com>
2023-08-02 17:15:59 +0100

    upd: #1278; build::manualChunks cont; cssJsChunk;

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import fs from 'fs';
 import viteCompression from 'vite-plugin-compression';
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 // import preload from "vite-plugin-preload";
@@ -27,76 +28,73 @@ export default defineConfig
       viteCompression(),
 
       // ### WARNING:
-      // ### 'this' plugin removes all 'CSS' from standard
-      // ### importing by 'svelte/svelteki' and styles will be missing
-      // ### if not imported as a '<link ... >' in the 'src/app.html'
-      // cssInjectedByJsPlugin
-      // (
-      //   {
+      cssInjectedByJsPlugin
+      (
+        {
 
-      //     // relativeCSSInjection: true,
+          // relativeCSSInjection: true,
 
-      //     // topExecutionPriority: true,
+          // topExecutionPriority: true,
 
-      //     // jsAssetsFilterFunction: function customJsAssetsfilterFunction
-      //     // (
-      //     //   outputChunk
-      //     // )
-      //     // {
+          // jsAssetsFilterFunction: function customJsAssetsfilterFunction
+          // (
+          //   outputChunk
+          // )
+          // {
 
-      //     //   // [🐞]
-      //     //   // ### NOTE:
-      //     //   // ### It appears, the 'outputChunk.filename' is of type:
-      //     //   // ### - _app/immutable/chunks/index.088b98a6.js
-      //     //   // ### - _app/immutable/chunks/index.8e8ca4ce.js
-      //     //   // ### etc.
-      //     //   // console.log(outputChunk.fileName);
+          //   // [🐞]
+          //   // ### NOTE:
+          //   // ### It appears, the 'outputChunk.filename' is of type:
+          //   // ### - _app/immutable/chunks/index.088b98a6.js
+          //   // ### - _app/immutable/chunks/index.8e8ca4ce.js
+          //   // ### etc.
+          //   // console.log(outputChunk.fileName);
 
-      //     //   return outputChunk.fileName == 'index.js';
-      //     // }
+          //   return outputChunk.fileName == 'index.js';
+          // }
 
-      //     // ### NOTE:
-      //     // ### definitive 'hack' solution for 'single CSS file' output chunk.
-      //     injectCode:
-      //     (
-      //       cssCode,
-      //       options
-      //     ): string =>
-      //     {
+          // ### NOTE:
+          // ### definitive 'hack' solution for 'single CSS file' output chunk.
+          injectCode:
+          (
+            cssCode,
+            options
+          ): string =>
+          {
 
-      //       const generateOneCssFile: boolean = false;
+            const generateOneCssFile: boolean = false;
 
-      //       if (generateOneCssFile)
-      //       {
-      //         // ### NOTE:
-      //         // ### the 'cssCode' generated contains some 'formatting' issues.
-      //         // ### remove 1st and last speech marks.
-      //         // ### remove cases of `\n` chars.
-      //         // ### correct custom case of 'ids'/'classes' using the 'forward-slash' in the declaration.
-      //         let cssCodeMod: string = cssCode.slice(1, -1);
-      //         cssCodeMod = cssCodeMod.replace(/\\n/g, "");
-      //         cssCodeMod = cssCodeMod.replace(/\\\\/g,"\\")
+            if (generateOneCssFile)
+            {
+              // ### NOTE:
+              // ### the 'cssCode' generated contains some 'formatting' issues.
+              // ### remove 1st and last speech marks.
+              // ### remove cases of `\n` chars.
+              // ### correct custom case of 'ids'/'classes' using the 'forward-slash' in the declaration.
+              let cssCodeMod: string = cssCode.slice(1, -1);
+              cssCodeMod = cssCodeMod.replace(/\\n/g, "");
+              cssCodeMod = cssCodeMod.replace(/\\\\/g,"\\")
 
-      //         // ### WARNING:
-      //         // ### 'all-css-chunk.css' must exist inside '/static'
-      //         fs.writeFile
-      //         (
-      //           './static/all-css-chunk.css',
-      //           cssCodeMod,
-      //           err =>
-      //           {
-      //             if (err) console.error(err);
-      //           }
-      //         );
-      //       }
+              // ### WARNING:
+              // ### 'all-css-chunk.css' must exist inside '/static'
+              fs.writeFile
+              (
+                './static/all-css-chunk.css',
+                cssCodeMod,
+                err =>
+                {
+                  if (err) console.error(err);
+                }
+              );
+            }
 
-      //       return '';
+            return '';
 
-      //       // return `try{if(typeof document != 'undefined'){var elementStyle = document.createElement('style');elementStyle.appendChild(document.createTextNode(${cssCode}));document.head.appendChild(elementStyle);}}catch(e){console.error('vite-plugin-css-injected-by-js', e);}`
-      //     }
+            // return `try{if(typeof document != 'undefined'){var elementStyle = document.createElement('style');elementStyle.appendChild(document.createTextNode(${cssCode}));document.head.appendChild(elementStyle);}}catch(e){console.error('vite-plugin-css-injected-by-js', e);}`
+          }
 
-      //   }
-      // ),
+        }
+      ),
     ],
 
     // ### DOC:
@@ -138,13 +136,13 @@ export default defineConfig
 
             // ### SEE:
             // ### https://github.com/sveltejs/kit/issues/7257#issuecomment-1528962348
-            if (id.includes('@sentry') && !id.includes('@sentry/browser') && !id.includes('@sentry/tracing'))
-              return 'vendor_sentry'
+            // if (id.includes('@sentry') && !id.includes('@sentry/browser') && !id.includes('@sentry/tracing'))
+            //   return 'vendor_sentry'
 
             // ### NOTE:
             // ### original
-            if (id.includes('node_modules'))
-              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            // if (id.includes('node_modules'))
+            //   return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
 
         }
