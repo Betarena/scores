72002bb0da32833dc4c023912d4df0335a7cd251
migbash <20924663+migbash@users.noreply.github.com>
2023-08-07 14:38:21 +0200

    chore: [[lang=lang]]/+page.svelte v6.5;

--- src/routes/[[lang=lang]]/+page.svelte
+++ src/routes/[[lang=lang]]/+page.svelte
@@ -1,11 +1,25 @@
-<!-- ===================
-	COMPONENT JS - BASIC
-=================== -->
+<!-- ===============
+### COMPONENT JS (w/ TS)
+### NOTE:
+### access custom Betarena Scores JS VScode Snippets by typing 'script...'
+================= -->
+
 <script lang="ts">
+
+  // #region ➤ 📦 Package Imports
+
 	import Layout from "$lib/components/page/home/Layout.svelte";
+
+  // #endregion ➤ 📦 Package Imports
+
 </script>
 
-<!-- ===================
-	COMPONENT HTML
-=================== -->
+<!-- ===============
+### COMPONENT HTML
+### NOTE:
+### use 'CTRL+SPACE' to autocomplete global class="" styles
+### NOTE:
+### access custom Betarena Scores VScode Snippets by typing emmet-like abbrev.
+================= -->
+
 <Layout />
