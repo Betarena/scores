fa0642a3654e8200f952b54f8c339fddc869e0de
migbash <20924663+migbash@users.noreply.github.com>
2023-08-29 10:20:40 +0100

    feat: competiton +page.svelte/+page.ts;

--- src/routes/[[lang=lang]]/[competitions=competitions]/[...competition_fill]/+page.ts
+++ src/routes/[[lang=lang]]/[competitions=competitions]/[...competition_fill]/+page.ts
@@ -1,9 +1,10 @@
 // #region ➤ 📦 Package Imports
 
 import { ERROR_CODE_PRELOAD, LAYOUT_1_LANG_PAGE_ERROR_MSG, dlog } from '$lib/utils/debug';
-import { PRELOAD_exitPage, promiseUrlsPreload, promiseValidUrlCheck } from '$lib/utils/platform-functions.js';
+import { PRELOAD_exitPage, promiseUrlsPreload } from '$lib/utils/platform-functions.js';
 
-import type { B_SAP_CP_T, B_SAP_D3 } from '@betarena/scores-lib/types/seo-pages.js';
+import type { B_SAP_CTP_D, B_SAP_CTP_T, B_SAP_D3 } from '@betarena/scores-lib/types/seo-pages.js';
+import type { B_COMP_MAIN_T } from '@betarena/scores-lib/types/types.competition.main.js';
 import type { ServerLoadEvent } from '@sveltejs/kit';
 import type { PageLoad } from '../$types.js';
 
@@ -22,13 +23,20 @@ export async function load
 
   const
   {
-    // url,
+    url,
     fetch,
     params,
     request,
-    setHeaders,
+    // setHeaders,
     // route
   } = event;
+
+  // ### [🐞]
+  // ### example: "/br/competicoes/predictor/lag-persita-kommer-att-vinna/31"
+  // console.log(url?.pathname);
+  // ### [🐞]
+  // ### example: "predictor/lag-persita-kommer-att-vinna/31"
+  // console.log(params?.competition_fill);
 
   const _langUrl: string =
     [undefined, 'en'].includes(params?.lang)
@@ -36,8 +44,19 @@ export async function load
       : params.lang
   ;
 
+  // ### [🐞]
+  // ### example: "es"
+  // console.log(_langUrl);
+
+  const competitionId = params?.competition_fill?.match(/\d+$/);
+  const COMPETITION_ID = parseInt(competitionId as unknown as string);
+
+  // ### [🐞]
+  // ### example: "31"
+  // console.log(competitionId);
+
   // **************************************
-  // VALIDATE URL                         *
+  // 📌 VALIDATE URL                      *
   // **************************************
 
   // const validUrlCheck: boolean = await promiseValidUrlCheck
@@ -64,20 +83,23 @@ export async function load
   //   );
   // }
 
-  const
+  let
   [
-    B_SAP_CP_T,
-    B_SAP_D3_CP_M
+    B_SAP_CTP_T,
+    B_SAP_CTP_D,
+    B_SAP_D3_CP_M,
+    B_COMP_MAIN_T
   ] = await fetchData
   (
     fetch,
-    _langUrl
+    _langUrl,
+    competitionId as unknown as string
   );
 
   // ### IMPORTANT
   // ### exit condition.
   const if_M_0: boolean =
-    B_SAP_CP_T == undefined
+    B_SAP_CTP_D == undefined
   ;
 	if (if_M_0)
   {
@@ -89,6 +111,13 @@ export async function load
       LAYOUT_1_LANG_PAGE_ERROR_MSG
     );
 	}
+
+  B_SAP_CTP_T = mutateSeoData
+  (
+    B_SAP_CTP_T,
+    B_SAP_CTP_D,
+    url?.pathname
+  );
 
   // ### [🐞]
   const t1: number = performance.now();
@@ -105,8 +134,11 @@ export async function load
     // @ts-expect-error <whatever>
     // ### NOTE: FIXME:
     // ### issues with setting correct <PageLoad> types.
-		B_SAP_CP_T,
-    B_SAP_D3_CP_M
+    COMPETITION_ID,
+		B_SAP_CTP_T,
+    B_SAP_CTP_D,
+    B_SAP_D3_CP_M,
+    B_COMP_MAIN_T
 	};
 
 }
@@ -121,13 +153,16 @@ export async function load
  */
 type PP_PROMISE_0 =
 [
-  B_SAP_CP_T | undefined,
-  B_SAP_D3 | undefined
+  B_SAP_CTP_T | undefined,
+  B_SAP_CTP_D | undefined,
+  B_SAP_D3 | undefined,
+  B_COMP_MAIN_T | undefined,
+  // B_COMP_MAIN_S | undefined
 ];
 
 /**
  * @summary
- * 🔹 HELPER
+ * 🔹 HELPER | IMPORTANT
  *
  * @description
  * TODO: DOC:
@@ -135,20 +170,24 @@ type PP_PROMISE_0 =
 async function fetchData
 (
   fetch: any,
-  _lang: string
+  _lang: string,
+  _competitionId: string
 ): Promise < PP_PROMISE_0 >
 {
 
-  // [🐞]
+  // ### [🐞]
   dlog
   (
-    `🚏 checkpoint ➤ src/routes/[[lang=lang]]/[competitions] fecthData`
+    `🚏 checkpoint ➤ src/routes/[[lang=lang]]/[competitions=competitions]/[...competition_fill] fecthData`
   );
 
   const urls_0: string[] =
   [
-    `/api/data/main/seo-pages?lang=${_lang}&page=competitions`,
+    `/api/data/main/seo-pages?lang=${_lang}&page=competition`,
+    `/api/data/main/seo-pages?competition_id=${_competitionId}&page=competition`,
     `/api/data/main/seo-pages?term=competitions`,
+    `/api/data/competition/main?lang=${_lang}`,
+    // `/api/data/competition?seo=true&lang=${_lang}`,
   ];
 
   const data_0: PP_PROMISE_0 = await promiseUrlsPreload
@@ -158,6 +197,64 @@ async function fetchData
   ) as PP_PROMISE_0;
 
   return data_0;
+}
+
+/**
+ * @summary
+ * 🔹 HELPER | IMPORTANT
+ *
+ * @param
+ * { B_SAP_CTP_T } data - competition (page) target - `seo` / `translations` data.
+ *
+ * @param
+ * { B_SAP_CTP_D } data2 - competition (page) target - critical data.
+ *
+ * @param
+ * { string } pathname - competition (page) target current pathname.
+ *
+ * @returns
+ * a mutated data `object`.
+ */
+function mutateSeoData
+(
+  data: B_SAP_CTP_T,
+  data2: B_SAP_CTP_D,
+  pathname: string
+): B_SAP_CTP_T
+{
+  let pageTitle: string = data?.general?.data?.title
+    .replace(/{ID}/g, data2?.data?.team_name)
+  ;
+  pageTitle += data2?.data?.target_competition_prediction;
+
+  data.main_data = JSON.parse
+  (
+		JSON.stringify(data?.main_data)
+      .replace('/{type}/{type_name}/{sport}/{title}-{prediction}/{id}', pathname)
+			.replace(/{title}/g, pageTitle)
+      .replace(/{prediction}/g, data2?.data?.creator_name)
+      .replace(/{creator}/g, data2?.data?.creator_name)
+	);
+
+	data.twitter_card = JSON.parse
+  (
+		JSON.stringify(data?.twitter_card)
+    .replace('/{type}/{type_name}/{sport}/{title}-{prediction}/{id}', pathname)
+    .replace(/{title}/g, pageTitle)
+    .replace(/{prediction}/g, data2?.data?.creator_name)
+    .replace(/{creator}/g, data2?.data?.creator_name)
+	);
+
+	data.opengraph = JSON.parse
+  (
+		JSON.stringify(data?.opengraph)
+    .replace('/{type}/{type_name}/{sport}/{title}-{prediction}/{id}', pathname)
+    .replace(/{title}/g, pageTitle)
+    .replace(/{prediction}/g, data2?.data?.creator_name)
+    .replace(/{creator}/g, data2?.data?.creator_name)
+	);
+
+  return data;
 }
 
 // #endregion ➤ 🛠️ METHODS
