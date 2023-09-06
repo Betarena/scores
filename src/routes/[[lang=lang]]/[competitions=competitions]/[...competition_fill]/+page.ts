b938798f82c05b215726713f8b940c07f5f2d79d
migbash <20924663+migbash@users.noreply.github.com>
2023-09-06 20:07:54 +0100

    #1563: fix cont. ;

--- src/routes/[[lang=lang]]/[competitions=competitions]/[...competition_fill]/+page.ts
+++ src/routes/[[lang=lang]]/[competitions=competitions]/[...competition_fill]/+page.ts
@@ -278,11 +278,11 @@ function mutateSeoData
     ?.replace(':', ' ')
   ;
   if (data2?.data?.target_competition_prediction == 'win')
-    pageTitle += data?.general?.data?.prediction?.[1];
+    pageTitle += data?.general?.data?.prediction?.['1'];
   else if (data2?.data?.target_competition_prediction == 'lose')
-    pageTitle += data?.general?.data?.prediction?.[2];
+    pageTitle += data?.general?.data?.prediction?.['2'];
   else
-    pageTitle += data?.general?.data?.prediction?.[3];
+    pageTitle += data?.general?.data?.prediction?.x;
   ;
 
   data.main_data = JSON.parse
