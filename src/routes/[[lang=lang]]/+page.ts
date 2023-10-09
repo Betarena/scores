809c13929a973e6f2e751b7a55c470fdd2bbd975
MigBash <20924663+migbash@users.noreply.github.com>
2023-10-09 10:06:33 +0100

    #1679 :: feat :: use of decompress +page.ts;

--- src/routes/[[lang=lang]]/+page.ts
+++ src/routes/[[lang=lang]]/+page.ts
@@ -1,17 +1,17 @@
 // #region ➤ 📦 Package Imports
 
-import { dlog, ERROR_CODE_INVALID, ERROR_CODE_PRELOAD, HOME_LANG_PAGE_ERROR_MSG, PAGE_INVALID_MSG } from '$lib/utils/debug';
-import { PRELOAD_invalid_data, promiseUrlsPreload, promiseValidUrlCheck } from '$lib/utils/platform-functions.js';
+import { dlog, ERROR_CODE_INVALID, PAGE_INVALID_MSG } from '$lib/utils/debug';
+import { promiseUrlsPreload, promiseValidUrlCheck } from '$lib/utils/platform-functions.js';
 import { error } from '@sveltejs/kit';
 
 import type { B_FEATB_T } from '@betarena/scores-lib/types/feat-betsite.js';
-import type { B_FEATM_S, B_FEATM_T } from '@betarena/scores-lib/types/feat-match.js';
-import type { B_LEGL_T } from '@betarena/scores-lib/types/league-list.js';
 import type { B_LEGT_T } from '@betarena/scores-lib/types/leagues-table.js';
 import type { B_LS2_S, B_LS2_T } from '@betarena/scores-lib/types/livescores-v2.js';
 import type { B_SEB_DT } from '@betarena/scores-lib/types/seo-block.js';
 import type { B_SAP_HP_T } from '@betarena/scores-lib/types/seo-pages.js';
 import type { B_TGOL_S, B_TGOL_T } from '@betarena/scores-lib/types/top-goalscorers.js';
+import type { B_FEATM_S, B_FEATM_T } from '@betarena/scores-lib/types/types.home.feat-match.js';
+import type { B_LEGL_S, B_LEGL_T } from '@betarena/scores-lib/types/types.home.league-list.js';
 import type { PageLoad } from './$types';
 
 // #endregion ➤ 📦 Package Imports
@@ -65,92 +65,36 @@ export async function load
 
   //#endregion [0] IMPORTANT EXTRACT URL DATA
 
-  //#region [0] IMPORTANT (PRE) PRE-LOAD DATA DOC: REF: [2]
+  // **************************************
+  //  📌 PREFETCH DATA                    *
+  // **************************************
 
-	const urls: string[] =
+  const
   [
-		// [ℹ] home (page)
-		`/api/data/main/seo-pages?lang=${urlLang}&page=homepage`,
-		// [ℹ] home (widgets)
-		`/api/data/home/feat-match?lang=${urlLang}`,
-		`/api/data/home/feat-match?lang=${urlLang}&seo=true`,
-		`/api/data/home/feat-betsite?lang=${urlLang}`,
-		`/api/data/home/top-goalscorers?lang=${urlLang}`,
-		`/api/data/home/top-goalscorers?lang=${urlLang}&seo=true`,
-		`/api/data/home/league-list?lang=${urlLang}`,
-		`/api/data/home/league-table?lang=${urlLang}`,
-		`/api/data/home/seo-block?lang=${urlLang}`,
-		`/api/data/home/livescores-v2?lang=${urlLang}`,
-		`/api/data/home/livescores-v2?seo=true&lang=${urlLang}`,
-	];
-
-  type HP_PROMISE =
-  [
-    B_SAP_HP_T | undefined,
-    B_FEATM_T | undefined,
-    B_FEATM_S | undefined,
-    B_FEATB_T | undefined,
-    B_TGOL_T | undefined,
-    B_TGOL_S | undefined,
-    B_LEGL_T | undefined,
-    B_LEGT_T | undefined,
-    B_SEB_DT | undefined,
-    B_LS2_T | undefined,
-    B_LS2_S | undefined
-  ];
-
-  const data = await promiseUrlsPreload
-  (
-    urls,
-    fetch
-  ) as HP_PROMISE;
-
-	const
-  [
-		PAGE_DATA_SEO,
+    PAGE_DATA_SEO,
 		B_FEATM_T,
     B_FEATM_S,
 		B_FEATB_T,
 		B_TGOL_T,
 		B_TGOL_S,
-		LEAGUE_LIST_WIDGET_DATA_SEO,
+		B_LEGL_T,
+		B_LEGL_S,
 		B_LEGT_T,
 		SEO_BLOCK_DATA,
     LIVESCORES_V2_T_DATA,
     LIVESCORES_V2_SEO
-	] = data;
-
-  //#endregion [0] IMPORTANT (PRE) PRE-LOAD DATA DOC: REF: [2]
-
-  //#region [3] IMPORTANT RETURN
-
-	const INVALID_PAGE_DATA_POINTS: boolean = data.includes(undefined);
-
-	// FIXME: currently based on checking for any NULL/UNDEFINED data points
-	// FIXME: should still allow for page access if page is VALID but widget data or
-	// FIXME: otherwise page component is simply missing
-	// [ℹ] exit;
-	if (INVALID_PAGE_DATA_POINTS)
-  {
-		throw error
-    (
-			ERROR_CODE_PRELOAD,
-			HOME_LANG_PAGE_ERROR_MSG
-		);
-	}
-
-  PRELOAD_invalid_data
+  ] = await fetchData
   (
-    data,
-    urls
+    fetch,
+    urlLang
   );
 
-  // [🐞]
+  // ### [🐞]
   const t1 = performance.now();
   dlog(`⏳ [HOME] preload ${((t1 - t0) / 1000).toFixed(2)} sec`, true)
 
-	// FIXME: types not working
 	return {
+	  // FIXME: types not working
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
     // NOTE: issues with setting correct <PageLoad> types,
@@ -162,15 +106,93 @@ export async function load
 		B_FEATB_T,
 		B_TGOL_T,
     B_TGOL_S,
-		LEAGUE_LIST_WIDGET_DATA_SEO,
+		B_LEGL_T,
+    B_LEGL_S,
 		B_LEGT_T,
 		SEO_BLOCK_DATA,
     LIVESCORES_V2_T_DATA,
     LIVESCORES_V2_SEO
 	};
-
-  //#endregion [3] IMPORTANT RETURN
-
 }
 
 // #endregion ➤ 🔄 LIFECYCLE [SVELTE]
+
+// #region ➤ 🛠️ METHODS
+
+/**
+ * @author
+ *  @migbash
+ * @summary
+ *  🔹 INTERFACE
+ * @description
+ *  📌 Target `types` for `Home Page` required at preload.
+ */
+type PP_PROMISE_0 =
+[
+  B_SAP_HP_T | undefined,
+  B_FEATM_T | undefined,
+  B_FEATM_S | undefined,
+  B_FEATB_T | undefined,
+  B_TGOL_T | undefined,
+  B_TGOL_S | undefined,
+  B_LEGL_T | undefined,
+  B_LEGL_S | undefined,
+  B_LEGT_T | undefined,
+  B_SEB_DT | undefined,
+  B_LS2_T | undefined,
+  B_LS2_S | undefined,
+];
+
+/**
+ * @author
+ *  @migbash
+ * @summary
+ *  🔹 HELPER
+ * @description
+ *  📌 Fetches target data for `_this_` page.
+ * @param { any } fetch
+ *  Target instance of `fetch` object.
+ * @param { string } _lang
+ *  Target `language`.
+ * @returns { Promise < PP_PROMISE_0 > }
+ */
+async function fetchData
+(
+  fetch: any,
+  _lang: string,
+): Promise < PP_PROMISE_0 >
+{
+
+  // ### [🐞]
+  dlog
+  (
+    `🚏 checkpoint [PRL] ➤ src/routes/[[lang=lang]] fecthData(..)`,
+    true
+  );
+
+  const urls_0: string[] =
+  [
+		`/api/data/main/seo-pages?lang=${_lang}&page=homepage`,
+		`/api/data/home/feat-match?lang=${_lang}&decompress`,
+		`/api/data/home/feat-match?lang=${_lang}&seo=true&decompress`,
+		`/api/data/home/feat-betsite?lang=${_lang}`,
+		`/api/data/home/top-goalscorers?lang=${_lang}`,
+		`/api/data/home/top-goalscorers?lang=${_lang}&seo=true`,
+		`/api/data/home/league-list?lang=${_lang}&decompress`,
+		`/api/data/home/league-list?lang=${_lang}&seo=true&decompress`,
+		`/api/data/home/league-table?lang=${_lang}`,
+		`/api/data/home/seo-block?lang=${_lang}`,
+		`/api/data/home/livescores-v2?lang=${_lang}&decompress`,
+		`/api/data/home/livescores-v2?seo=true&lang=${_lang}&decompress`,
+  ];
+
+  const data_0: PP_PROMISE_0 = await promiseUrlsPreload
+  (
+    urls_0,
+    fetch,
+  ) as PP_PROMISE_0;
+
+  return data_0;
+}
+
+// #endregion ➤ 🛠️ METHODS
