367fc11cffa8cf36119a436ba9bed861a3c84e68
migbash <20924663+migbash@users.noreply.github.com>
2023-11-04 23:31:20 +0100

    feat :: #1802

--- src/lib/types/global.d.ts
+++ src/lib/types/global.d.ts
@@ -17,4 +17,24 @@ interface Window
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
   /** @description `window` | `document` global Ethereum (MetaMask) Methods access. */
 	ethereum: any;
+  /** @description `window` | `document` global Intercom Properties access. */
+  intercomSettings:
+  {
+    /** @description client/user api-base */
+    api_base: string;
+    /** @description intercom `app-id` */
+    app_id: string;
+    /** @description client/user full name */
+    name?: string | undefined;
+    /** @description client/user email */
+    email?: stirng | undefined;
+    /** @description client/user created at date */
+    created_at?: string | undefined;
+    /** @description client/user `UID` */
+    uid?: string;
+    /** @description client/user platform language */
+    lang?: string;
+    /** @description client/user platform language */
+    competition_number?: number;
+  };
 }
