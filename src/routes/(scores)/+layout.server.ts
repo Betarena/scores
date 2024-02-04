7880731f32ba1294371f9504308eebf2e92d2982
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2024-02-04 22:53:20 +0300

    deepsource fix

--- src/routes/(scores)/+layout.server.ts
+++ src/routes/(scores)/+layout.server.ts
@@ -1,6 +1,3 @@
 import type { LayoutServerLoad } from '../$types';
 
-export const load: LayoutServerLoad = async ({ parent }) => {
-  var parentLayoutData = await parent();
-  return parentLayoutData;
-};
+export const load: LayoutServerLoad = ({ parent }) => parent();
