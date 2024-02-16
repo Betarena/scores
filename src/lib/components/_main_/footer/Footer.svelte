efb454cfab37d4c6f1af75e409ae037e57837d50
MigBash <20924663+migbash@users.noreply.github.com>
2024-02-16 10:14:56 +0100

    fix(foter): corrections

--- src/lib/components/_main_/footer/Footer.svelte
+++ src/lib/components/_main_/footer/Footer.svelte
@@ -31,9 +31,9 @@
 
   import SeoBox from '$lib/components/SEO-Box.svelte';
 
+	import TranslationText from '$lib/components/misc/Translation-Text.svelte';
 	import type { B_H_SFOOTD_Social_Network } from '@betarena/scores-lib/types/_HASURA_.js';
 	import type { B_FOT_T } from '@betarena/scores-lib/types/types.main.footer.js';
-  import TranslationText from '$lib/components/misc/Translation-Text.svelte';
 
   // #endregion ➤ 📦 Package Imports
 
@@ -53,50 +53,50 @@
 
   const
     /** @description 📌 `this` component **main** `id` and `data-testid` prefix. */
-    CNAME = 'global⮕footer⮕w⮕main',
+    CNAME = 'global⮕footer⮕w⮕main'
     /** @description 📱 MOBILE */
-    VIEWPORT_MOBILE_INIT = 710,
+    ,VIEWPORT_MOBILE_INIT = 710
     /** @description 💻 TABLET */
-    VIEWPORT_TABLET_INIT = 1160
+    ,VIEWPORT_TABLET_INIT = 1160
   ;
 
   let
-    isViewMobile: boolean = true,
-    isViewTablet: boolean = true,
-    B_FOT_T: B_FOT_T = $page.data?.B_FOT_T,
-    homepageURL: string,
-    logoLink: string,
-    begambleawareorg: string,
-    logo_full: string,
-    legal18icon: string,
-    icon_discord: string,
-    icon_linkedin: string,
-    icon_medium: string,
-    icon_telegram: string,
-    icon_x: string,
-    icon_github: string,
-    socialNetworkOrder: B_H_SFOOTD_Social_Network[] =
-    [
-      'discord',
-      'telegram',
-      'x',
-      'medium',
-      'linkedin',
-      'github'
+    isViewMobile: boolean = true
+    ,isViewTablet: boolean = true
+    ,B_FOT_T: B_FOT_T = $page.data.B_FOT_T
+    ,homepageURL: string
+    ,logoLink: string
+    ,begambleawareorg: string
+    ,logo_full: string
+    ,legal18icon: string
+    ,icon_discord: string
+    ,icon_linkedin: string
+    ,icon_medium: string
+    ,icon_telegram: string
+    ,icon_x: string
+    ,icon_github: string
+    ,socialNetworkOrder: B_H_SFOOTD_Social_Network[]
+    = [
+      'discord'
+      ,'telegram'
+      ,'x'
+      ,'medium'
+      ,'linkedin'
+      ,'github'
     ]
   ;
 
-  $: homepageURL =
-    $sessionStore?.serverLang != 'en'
+  $: homepageURL
+    = $sessionStore.serverLang != 'en'
       ? `/${$page.params.lang}`
-      : `/`
+      : '/'
   ;
-  $: logoLink =
-    $sessionStore?.serverLang != 'en'
-      ? `${$page.url.origin}/${$sessionStore?.serverLang}`
+  $: logoLink
+    = $sessionStore.serverLang != 'en'
+      ? `${$page.url.origin}/${$sessionStore.serverLang}`
       : $page.url.origin
   ;
-  $: B_FOT_T: B_FOT_T = $page.data?.B_FOT_T;
+  $: B_FOT_T = $page.data.B_FOT_T;
 
   // #endregion ➤ 📌 VARIABLES
 
@@ -116,13 +116,12 @@
 	 * @description reload current page;
 	 */
 	function reloadPage
-  (
-  )
-  {
-		if ($page.url.pathname.split('/').length - 1 ==	1)
-    {
-			window.location.reload();
-		}
+	(
+	)
+	{
+	  if ($page.url.pathname.split('/').length - 1 ==	1)
+
+	    window.location.reload();
 	}
 
   /**
@@ -197,12 +196,12 @@
           [
             isViewTablet,
             isViewMobile
-          ] =
-          viewport_change
-          (
-            VIEWPORT_TABLET_INIT,
-            VIEWPORT_MOBILE_INIT
-          );
+          ]
+          = viewport_change
+            (
+              VIEWPORT_TABLET_INIT,
+              VIEWPORT_MOBILE_INIT
+            );
         }
       );
     }
@@ -233,37 +232,37 @@ FOOTER | SEO
   SOCIAL LINKS
   -->
   <p>
-    {B_FOT_T?.links?.latest_news}
+    {B_FOT_T.links.latest_news}
   </p>
   <p>
-    {B_FOT_T?.links?.about_us}
+    {B_FOT_T.links.about_us}
   </p>
   <p>
-    {B_FOT_T?.links?.betting_tips}
+    {B_FOT_T.links.betting_tips}
   </p>
   <p>
-    {B_FOT_T?.links?.privacy}
+    {B_FOT_T.links.privacy}
   </p>
   <p>
-    {B_FOT_T?.links?.social_networks}
+    {B_FOT_T.links.social_networks}
   </p>
   <p>
-    {B_FOT_T?.links?.terms}
+    {B_FOT_T.links.terms}
   </p>
   <p>
-    {B_FOT_T?.links?.status}
+    {B_FOT_T.links.status}
   </p>
   <p>
-    {B_FOT_T?.links?.changelog}
+    {B_FOT_T.links.changelog}
   </p>
 
 
   <!--
   SOCIAL LINKS - 2
   -->
-  {#each Object.keys(B_FOT_T?.links?.social_networks) ?? [] as key}
+  {#each Object.keys(B_FOT_T.links.social_networks) ?? [] as key}
     <p>
-      {B_FOT_T?.links?.social_networks?.[key]}
+      {B_FOT_T.links.social_networks[key]}
     </p>
   {/each}
 
@@ -283,7 +282,7 @@ FOOTER | Main Component
     -->
     <div
       id="{CNAME}⮕brand"
-      on:click={() => reloadPage()}
+      on:click={() => {return reloadPage()}}
       on:keypress={(e) => { if (e.key === 'Enter') reloadPage() }}
     >
 
@@ -337,7 +336,7 @@ FOOTER | Main Component
         m-b-20
         "
       >
-        {B_FOT_T?.terms?.follow ?? 'Follow us'}
+        {B_FOT_T.terms.follow ?? 'Follow us'}
       </p>
 
       <!--
@@ -362,14 +361,14 @@ FOOTER | Main Component
           <a
             rel="external"
             target="_blank"
-            href={B_FOT_T?.links?.social_networks?.[key]}
+            href={B_FOT_T.links.social_networks[key]}
           >
             {#if icon_github}
               <img
                 loading="lazy"
                 src={getSocialIcon(key)}
-                alt="{key?.toLowerCase()}-icon"
-                title="{key?.toLowerCase()}-icon"
+                alt="{key.toLowerCase()}-icon"
+                title="{key.toLowerCase()}-icon"
                 width=32
                 height=32
               />
@@ -404,7 +403,7 @@ FOOTER | Main Component
       >
         <TranslationText
           key={`${CNAME}/newsletter-box`}
-          text={B_FOT_T?.terms?.subscribe_newsletter}
+          text={B_FOT_T.terms.subscribe_newsletter}
           fallback={'Subscribe to newsletter'}
         />
       </p>
@@ -435,7 +434,7 @@ FOOTER | Main Component
           w-500
           "
         >
-          {B_FOT_T?.terms?.subscribe_cta}
+          {B_FOT_T.terms.subscribe_cta}
         </p>
       </button>
 
@@ -463,7 +462,7 @@ FOOTER | Main Component
         >
           <a
             rel="external"
-            href={B_FOT_T?.links?.latest_news}
+            href={B_FOT_T.links.latest_news}
           >
             <p
               class=
@@ -474,7 +473,7 @@ FOOTER | Main Component
               hover-color-primary
               "
             >
-              {B_FOT_T?.terms?.latest_news}
+              {B_FOT_T.terms.latest_news}
             </p>
           </a>
         </li>
@@ -506,7 +505,7 @@ FOOTER | Main Component
         >
           <a
             rel="external"
-            href={B_FOT_T?.links?.about_us}
+            href={B_FOT_T.links.about_us}
           >
             <p
               class=
@@ -517,7 +516,7 @@ FOOTER | Main Component
               hover-color-primary
               "
             >
-              {B_FOT_T?.terms?.about_us}
+              {B_FOT_T.terms.about_us}
             </p>
           </a>
         </li>
@@ -549,7 +548,7 @@ FOOTER | Main Component
         >
           <a
             rel="external"
-            href={B_FOT_T?.links?.terms}
+            href={B_FOT_T.links.terms}
           >
             <p
               class=
@@ -560,7 +559,7 @@ FOOTER | Main Component
               hover-color-primary
               "
             >
-              {B_FOT_T?.terms?.terms}
+              {B_FOT_T.terms.terms}
             </p>
           </a>
         </li>
@@ -596,7 +595,7 @@ FOOTER | Main Component
         >
           <a
             rel="external"
-            href={B_FOT_T?.links?.privacy}
+            href={B_FOT_T.links.privacy}
           >
             <p
               class=
@@ -607,7 +606,7 @@ FOOTER | Main Component
               hover-color-primary
               "
             >
-              {B_FOT_T?.terms?.privacy}
+              {B_FOT_T.terms.privacy}
             </p>
           </a>
         </li>
@@ -639,7 +638,7 @@ FOOTER | Main Component
         >
           <a
             rel="external"
-            href={B_FOT_T?.links?.status}
+            href={B_FOT_T.links.status}
           >
             <p
               class=
@@ -650,7 +649,7 @@ FOOTER | Main Component
               hover-color-primary
               "
             >
-              {B_FOT_T?.terms?.status}
+              {B_FOT_T.terms.status}
             </p>
           </a>
         </li>
@@ -682,7 +681,7 @@ FOOTER | Main Component
         >
           <a
             rel="external"
-            href={B_FOT_T?.links?.changelog}
+            href={B_FOT_T.links.changelog}
           >
             <p
               class=
@@ -693,7 +692,7 @@ FOOTER | Main Component
               hover-color-primary
               "
             >
-              {B_FOT_T?.terms?.changelog}
+              {B_FOT_T.terms.changelog}
             </p>
           </a>
         </li>
@@ -811,99 +810,103 @@ FOOTER | Main Component
 		width: -webkit-fill-available;
 		padding: 48px 18px;
 
-	  div#global⮕footer⮕w⮕main⮕inner
+    div#global⮕footer⮕w⮕main
     {
-      /* 📌 position */
-      display: grid;
-      gap: 32px 85px;
-      grid-template-columns: 1fr;
-      align-items: start;
-      justify-items: stretch;
-      /* 🎨 style */
-      max-width: 1378px;
-      text-align: center;
-    }
 
-    div#global⮕footer⮕w⮕main⮕brand
-    {
-      /* 📌 position */
-      grid-column: 1;
-      grid-row: 1;
-      /* 🎨 style */
-      justify-self: center;
-    }
-    div#global⮕footer⮕w⮕main⮕brand img
-    {
-      /* 🎨 style */
-      height: 32px;
-      width: 151px;
-    }
+      &⮕inner
+      {
+        /* 📌 position */
+        display: grid;
+        gap: 32px 85px;
+        grid-template-columns: 1fr;
+        align-items: start;
+        justify-items: stretch;
+        /* 🎨 style */
+        max-width: 1378px;
+        text-align: center;
+      }
 
-    div#global⮕footer⮕w⮕main⮕social-box
-    {
-      /* 📌 position */
-      grid-column: 1;
-      grid-row: 2;
-    }
-    div#global⮕footer⮕w⮕main⮕social-media-box
-    {
-      /* 🎨 style */
-      gap: 16px;
-      grid-auto-flow: column;
-      width: 100%;
-    }
+      &⮕brand
+      {
+        /* 📌 position */
+        grid-column: 1;
+        grid-row: 1;
+        /* 🎨 style */
+        justify-self: center;
+      }
+      &⮕brand img
+      {
+        /* 🎨 style */
+        height: 32px;
+        width: 151px;
+      }
 
-    div#global⮕footer⮕w⮕main⮕newsletter-box
-    {
-      /* 📌 position */
-      grid-column: 1;
-      grid-row: 3;
-    }
-    div#global⮕footer⮕w⮕main⮕newsletter-box button#newsletter-subscribe-btn
-    {
-      /* 🎨 style */
-      height: 44px;
-      width: 100%;
-    }
+      &⮕social-box
+      {
+        /* 📌 position */
+        grid-column: 1;
+        grid-row: 2;
+      }
+      &⮕social-media-box
+      {
+        /* 🎨 style */
+        gap: 16px;
+        grid-auto-flow: column;
+        width: 100%;
+      }
 
-    div#global⮕footer⮕w⮕main⮕menu-list
-    {
-      /* 📌 position */
-      grid-column: 1;
-      grid-row: 4;
-    }
-    div#global⮕footer⮕w⮕main⮕menu-list ul
-    {
-      /* 📌 position */
-      display: flex;
-      /* 🎨 style */
-      list-style-type: none;
-      padding: 0;
-      margin: 0;
-      place-content: center;
-    }
-    div#global⮕footer⮕w⮕main⮕menu-list div.menu-separator
-    {
-      /* 🎨 style */
-      width: 1px;
-      height: 16px;
-      background-color: #4b4b4b;
-    }
+      &⮕newsletter-box
+      {
+        /* 📌 position */
+        grid-column: 1;
+        grid-row: 3;
+      }
+      &⮕newsletter-box button#newsletter-subscribe-btn
+      {
+        /* 🎨 style */
+        height: 44px;
+        width: 100%;
+      }
 
-    div#global⮕footer⮕w⮕main⮕legal-bet-box
-    {
-      /* 📌 position */
-      grid-column: 1;
-      grid-row: 5;
-      /* 🎨 style */
-      justify-content: center;
-    }
+      &⮕menu-list
+      {
+        /* 📌 position */
+        grid-column: 1;
+        grid-row: 4;
+      }
+      &⮕menu-list ul
+      {
+        /* 📌 position */
+        display: flex;
+        /* 🎨 style */
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+        place-content: center;
+      }
+      &⮕menu-list div.menu-separator
+      {
+        /* 🎨 style */
+        width: 1px;
+        height: 16px;
+        background-color: #4b4b4b;
+      }
 
-    div#global⮕footer⮕w⮕main⮕company-box
-    {
-      /* 📌 position */
-      grid-column: 1;
-      grid-row: 6;
+      &⮕legal-bet-box
+      {
+        /* 📌 position */
+        grid-column: 1;
+        grid-row: 5;
+        /* 🎨 style */
+        justify-content: center;
+      }
+
+      &⮕company-box
+      {
+        /* 📌 position */
+        grid-column: 1;
+        grid-row: 6;
+      }
     }
   }
 
@@ -921,69 +924,72 @@ FOOTER | Main Component
     {
 			height: 416px;
 			padding: 68px 34px;
-		}
 
-    div#global⮕footer⮕w⮕main⮕inner
-    {
-      /* 📌 position */
-      display: grid;
-      gap: 32px 85px;
-      grid-template-columns: repeat(2, auto);
-      grid-template-rows: repeat(4, auto);
-      justify-items: start;
-      align-items: initial;
-      text-align: initial;
-      justify-content: space-between;
-		}
+      div#global⮕footer⮕w⮕main
+      {
+        &⮕inner
+        {
+          /* 📌 position */
+          display: grid;
+          gap: 32px 85px;
+          grid-template-columns: repeat(2, auto);
+          grid-template-rows: repeat(4, auto);
+          justify-items: start;
+          align-items: initial;
+          text-align: initial;
+          justify-content: space-between;
+        }
 
-    div#global⮕footer⮕w⮕main⮕brand
-    {
-      /* 🎨 style */
-      justify-self: unset;
-    }
+        &⮕brand
+        {
+          /* 🎨 style */
+          justify-self: unset;
+        }
 
-    div#global⮕footer⮕w⮕main⮕social-box
-    {
-      /* 📌 position */
-      grid-column: 1;
-      grid-row: 2;
-    }
+        &⮕social-box
+        {
+          /* 📌 position */
+          grid-column: 1;
+          grid-row: 2;
+        }
 
-		div#global⮕footer⮕w⮕main⮕newsletter-box
-    {
-      /* 📌 position */
-      grid-column: 2;
-      grid-row: 2;
-      /* 🎨 style */
-			width: 250px;
-      height: fit-content;
-		}
+        &⮕newsletter-box
+        {
+          /* 📌 position */
+          grid-column: 2;
+          grid-row: 2;
+          /* 🎨 style */
+          width: 250px;
+          height: fit-content;
+        }
 
-    div#global⮕footer⮕w⮕main⮕menu-list
-    {
-      /* 📌 position */
-      grid-column: 1 / 3;
-      grid-row: 3;
-    }
-		div#global⮕footer⮕w⮕main⮕menu-list ul
-    {
-			place-content: unset;
-		}
+        &⮕menu-list
+        {
+          /* 📌 position */
+          grid-column: 1 / 3;
+          grid-row: 3;
+        }
+        &⮕menu-list ul
+        {
+          place-content: unset;
+        }
 
-    div#global⮕footer⮕w⮕main⮕company-box
-    {
-      /* 📌 position */
-      grid-column: 1;
-      grid-row: 4;
-    }
+        &⮕company-box
+        {
+          /* 📌 position */
+          grid-column: 1;
+          grid-row: 4;
+        }
 
-    div#global⮕footer⮕w⮕main⮕legal-bet-box
-    {
-      /* 📌 position */
-      grid-column: 2;
-      grid-row: 4;
-      /* 🎨 style */
-      justify-self: right;
+        &⮕legal-bet-box
+        {
+          /* 📌 position */
+          grid-column: 2;
+          grid-row: 4;
+          /* 🎨 style */
+          justify-self: right;
+        }
+      }
     }
 
 	}
@@ -1000,83 +1006,86 @@ FOOTER | Main Component
 			text-align: center;
 			text-align: -webkit-center;
 			text-align: -moz-center;
-		}
 
-		/*
-    ▓▓ inner container */
-    div#global⮕footer⮕w⮕main⮕inner
-    {
-      /* 📌 position */
-			display: grid;
-      gap: 17px 85px;
-      grid-template-columns: repeat(3, auto);
-      grid-template-rows: repeat(2, auto);
-			justify-content: space-between;
-			justify-items: stretch;
-      align-items: end;
-		}
+      div#global⮕footer⮕w⮕main
+      {
+        /*
+        ▓▓ inner container */
+        &⮕inner
+        {
+          /* 📌 position */
+          display: grid;
+          gap: 17px 85px;
+          grid-template-columns: repeat(3, auto);
+          grid-template-rows: repeat(2, auto);
+          justify-content: space-between;
+          justify-items: stretch;
+          align-items: end;
+        }
 
-    /*
-    ▓▓ platform menu list */
-    div#global⮕footer⮕w⮕main⮕menu-list
-    {
-      /* 📌 position */
-      grid-column: 2;
-      grid-row: 2;
-    }
+        /*
+        ▓▓ platform menu list */
+        &⮕menu-list
+        {
+          /* 📌 position */
+          grid-column: 2;
+          grid-row: 2;
+        }
 
-    /*
-    ▓▓ newsletter box */
-    div#global⮕footer⮕w⮕main⮕newsletter-box
-    {
-      /* 📌 position */
-      grid-column: 3;
-      grid-row: 1;
-      /* 🎨 style */
-			width: 100%;
-		}
-		div#global⮕footer⮕w⮕main⮕newsletter-box button#newsletter-subscribe-btn
-    {
-      /* 🎨 style */
-			padding: 11.5px 23.5px;
-      min-width: 328px;
-		}
+        /*
+        ▓▓ newsletter box */
+        &⮕newsletter-box
+        {
+          /* 📌 position */
+          grid-column: 3;
+          grid-row: 1;
+          /* 🎨 style */
+          width: 100%;
+        }
+        &⮕newsletter-box button#newsletter-subscribe-btn
+        {
+          /* 🎨 style */
+          padding: 11.5px 23.5px;
+          min-width: 328px;
+        }
 
-    /*
-    ▓▓ social box */
-    div#global⮕footer⮕w⮕main⮕social-box
-    {
-      /* 📌 position */
-      grid-column: 2;
-      grid-row: 1;
-    }
-    div#global⮕footer⮕w⮕main⮕social-media-box
-    {
-      /* 🎨 style */
-			justify-content: left;
-      gap: 16px;
-      grid-auto-flow: column;
-      width: 100%;
-    }
+        /*
+        ▓▓ social box */
+        &⮕social-box
+        {
+          /* 📌 position */
+          grid-column: 2;
+          grid-row: 1;
+        }
+        &⮕social-media-box
+        {
+          /* 🎨 style */
+          justify-content: left;
+          gap: 16px;
+          grid-auto-flow: column;
+          width: 100%;
+        }
 
-    /*
-    ▓▓ company box */
-    div#global⮕footer⮕w⮕main⮕company-box
-    {
-      /* 📌 position */
-      grid-column: 1;
-      grid-row: 2;
-    }
+        /*
+        ▓▓ company box */
+        &⮕company-box
+        {
+          /* 📌 position */
+          grid-column: 1;
+          grid-row: 2;
+        }
 
-    /*
-    ▓▓ legal bet box */
-    div#global⮕footer⮕w⮕main⮕legal-bet-box
-    {
-      /* 📌 position */
-      grid-column: 3;
-      grid-row: 2;
-      /* 🎨 style */
-      justify-self: left;
+        /*
+        ▓▓ legal bet box */
+        &⮕legal-bet-box
+        {
+          /* 📌 position */
+          grid-column: 3;
+          grid-row: 2;
+          /* 🎨 style */
+          justify-self: left;
+        }
+      }
     }
 
 	}
