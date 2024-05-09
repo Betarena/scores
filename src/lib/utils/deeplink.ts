5765b6da0e584e33d25ba82c6998a952a70c49da
Ivan Izobov <54995954+Izobov@users.noreply.github.com>
2024-05-09 02:08:02 +0000

    fix redirect

--- src/lib/utils/deeplink.ts
+++ src/lib/utils/deeplink.ts
@@ -20,10 +20,14 @@ import { isSignInWithEmailLink, signInWithCustomToken, signInWithEmailLink } fro
 import { auth } from '$lib/firebase/init.js';
 import { setCookie } from '$lib/store/cookie.js';
 import sessionStore from '$lib/store/session.js';
+import userBetarenaSettings from '$lib/store/user-settings.js';
 import { authWithMoralis, successAuthComplete } from './authentication.js';
 import { AU_W_TAG, dlog, dlogv2, errlog } from './debug.js';
 
 import type { Page } from '@sveltejs/kit';
+import { routeIdHome } from '$lib/constants/paths.js';
+import type { B_NAV_T } from '@betarena/scores-lib/types/navbar.js';
+import { generateUrlCompetitions } from './string.js';
 
 // #endregion ➤ 📦 Package Imports
 
@@ -51,13 +55,35 @@ export async function mainDeepLinkCheck
      * @description
      * 📝 Data for `page`.
      */
-    page = sessionStore.extract<Page>('page'),
-    /**
-     * @description
-     * 📝 Data for `page`.
-     */
-    revertUrl = `${page?.url.origin}${page?.url.pathname}`
-  ;
+    page = sessionStore.extract<Page>('page')
+    ;
+  let     /**
+  * @description
+  * 📝 Data for `page`.
+  */
+ revertUrl = `${page?.url.origin}${page?.url.pathname}`
+  if (page?.route.id === routeIdHome)
+  {
+    const lang = sessionStore.extract<string>('lang');
+    const [preferedPage] = userBetarenaSettings.extract<{buttons_order: string[]}>('user')?.buttons_order || ["scores"];
+    const translations = page.data.B_NAV_T as B_NAV_T | null | undefined;
+    switch (preferedPage) {
+      case "competitions":
+        revertUrl = generateUrlCompetitions(
+          lang || "",
+          page.data.B_SAP_D3_CP_H
+        );
+        break;
+      case "content":
+        revertUrl =
+        translations?.scores_header_translations?.section_links
+        ?.sports_content_url as string;
+        break;
+        case "scores":
+      default:
+        revertUrl = `/${lang === "en" ? "" : `${lang}/`}scores`;
+      }
+  }
 
   await goto
   (
--- src/routes/(scores)/[[lang=lang]]/+page.svelte
+++ src/routes/(scores)/[[lang=lang]]/+page.svelte
@@ -42,12 +42,13 @@
   import { goto } from "$app/navigation";
   import { page } from "$app/stores";
   import { generateUrlCompetitions } from "$lib/utils/string.js";
+  import { browser } from "$app/environment";
+    import type { B_NAV_T } from "@betarena/scores-lib/types/navbar.js";
 
   $: ({ serverLang } = $sessionStore);
   $: [preferedPage] = $userBetarenaSettings.user?.scores_user_data
     ?.buttons_order || ["scores"];
   $: trsanslationData = $page.data.B_NAV_T as B_NAV_T | null | undefined;
-  let isRedirect = false;
   $: {
     let url = $page.url.pathname;
     switch (preferedPage) {
@@ -59,27 +60,24 @@
         break;
       case "content":
         url =
-        trsanslationData?.scores_header_translations?.section_links
-        ?.sports_content_url;
+          trsanslationData?.scores_header_translations?.section_links
+            ?.sports_content_url;
         break;
-        case "scores":
+      case "scores":
       default:
         url = `/${serverLang === "en" ? "" : `${serverLang}/`}scores`;
-      }
-      if (!isRedirect)  {
-        goto(url);
-        isRedirect = true;
-      }
+    }
+    if (browser) {
+      goto(url);
+    }
   }
   // #endregion ➤ 📦 Package Imports
 </script>
 
 <section />
 
-
 <style>
   section {
     height: 100%;
   }
 </style>
-
