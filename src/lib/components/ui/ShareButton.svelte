2cf191bab4e9bc2bc3ead82bb2ad58b6631fc9e5
Ivan Izobov <54995954+Izobov@users.noreply.github.com>
2024-07-18 03:24:39 +0000

    dev: colors

---
+++ src/lib/components/ui/ShareButton.svelte
@@ -0,0 +1,23 @@
+<script>
+    import ShareIcon from "./assets/share-icon.svelte";
+
+
+    import Button from "./Button.svelte";
+
+</script>
+<Button type="secondary" id="share-button" classname="share-button" style="width: 40px; height: 40px; padding: 0">
+  <ShareIcon />
+</Button>
+
+<style lang="scss">
+  :global(.share-button#share-button) {
+    padding: 0;
+    width: 40px;
+    height: 40px;
+
+    &:hover {
+      --text-color: #fff
+    }
+
+  }
+</style>
