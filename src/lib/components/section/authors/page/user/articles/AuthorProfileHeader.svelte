71d7b9ee9f83440029c7b200ffef43b5933e9e9f
Ivan Izobov <54995954+Izobov@users.noreply.github.com>
2024-07-22 15:38:36 +0000

    dev: Update size of StackedAvatars in AuthorProfileHeader.svelte

--- src/lib/components/section/authors/page/user/articles/AuthorProfileHeader.svelte
+++ src/lib/components/section/authors/page/user/articles/AuthorProfileHeader.svelte
@@ -203,7 +203,7 @@
         <a href={getLink("subscribers")} class="followers">
           <StackedAvatars
             src={subscribers_profiles.map((u) => u.profile_photo || "")}
-            size={viewportType === "desktop" ? 31 : 24}
+            size={viewportType === "desktop" ? 32 : 24}
           />
           <div class="followers-names">
             <span class="subscribed_by">
