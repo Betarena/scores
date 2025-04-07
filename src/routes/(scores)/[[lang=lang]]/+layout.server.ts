7d3931f3a6257a3f01535d01a34d3cc0f7554130
Ivan Izobov <54995954+Izobov@users.noreply.github.com>
2025-04-07 13:48:12 +0000

    fix: add language redirection logic in layout server

--- src/routes/(scores)/[[lang=lang]]/+layout.server.ts
+++ src/routes/(scores)/[[lang=lang]]/+layout.server.ts
@@ -1,9 +1,19 @@
 import type { LayoutServerLoad } from './$types';
 import { main } from '$lib/sveltekit/load/load.author.layout.js';
 import { dlogv2 } from '$lib/utils/debug.js';
+import { redirect } from '@sveltejs/kit';
 
-export const load = (async ({ parent, fetch, depends }) =>
+export const load = (async ({ parent, fetch, depends, params, url }) =>
 {
+  if (params.lang && url.pathname !== `/${params.lang}`)
+  {
+    throw redirect(301, `/${params.lang}`);
+  }
+
+  if (!params.lang && url.pathname !== '/')
+  {
+    throw redirect(301, '/');
+  }
   const data = await parent();
   const { langParam } = data;
   // [🐞]
