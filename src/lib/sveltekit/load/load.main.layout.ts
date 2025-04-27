d4702e8fe2360bc8dac38a86c5aa0cad0af490b7
MigBash <20924663+migbash@users.noreply.github.com>
2025-04-27 21:51:58 +0200

    refactor(load): update
    [1] ────
    Update '+layout.server.ts' module for latest format & structure.
    [2] ────
    Update 'load.main.layout.ts' module for latest format & structure.

--- src/lib/sveltekit/load/load.main.layout.ts
+++ src/lib/sveltekit/load/load.main.layout.ts
@@ -1,31 +1,49 @@
 // ╭──────────────────────────────────────────────────────────────────────────────────╮
-// │ 📌 High Order Component Overview                                                 │
+// │ 📌 High Order Overview                                                           │
 // ┣──────────────────────────────────────────────────────────────────────────────────┫
-// │ ➤ Internal Svelte Code Format :|: V.8.0                                          │
-// │ ➤ Status :|: 🔒 LOCKED                                                           │
-// │ ➤ Author(s) :|: @migbash                                                         │
+// │ ➤ Code Format   // V.8.0                                                         │
+// │ ➤ Status        // 🔒 LOCKED                                                     │
+// │ ➤ Author(s)     // @migbash                                                      │
+// │ ➤ Maintainer(s) // @migbash                                                      │
+// │ ➤ Created on    // <date-created>                                                │
 // ┣──────────────────────────────────────────────────────────────────────────────────┫
 // │ 📝 Description                                                                   │
 // ┣──────────────────────────────────────────────────────────────────────────────────┫
-// │ Main Scores Platform Layout Loader ('Server-Side')                               │
+// │ BETARENA (Module)
+// │ |: <insert-module-summary-here>
 // ╰──────────────────────────────────────────────────────────────────────────────────╯
 
 // #region ➤ 📦 Package Imports
 
 import { performance } from 'perf_hooks';
 
-import { ERROR_CODE_PRELOAD, LAYOUT_1_LANG_PAGE_ERROR_MSG, dlog, dlogv2 } from '$lib/utils/debug';
+import { ERROR_CODE_PRELOAD, LAYOUT_1_LANG_PAGE_ERROR_MSG, dlogv2 } from '$lib/utils/debug';
 import { detectDeviceWithUA } from '$lib/utils/device.js';
 import { detectPlatformLanguage } from '$lib/utils/languages.js';
 import { preloadExitLogic, promiseUrlsPreload } from '$lib/utils/navigation.js';
+import { parseObject } from '$lib/utils/string.2.js';
 
 import type { IAuthTrs } from '@betarena/scores-lib/types/auth.js';
 import type { B_NAV_T } from '@betarena/scores-lib/types/navbar.js';
-import type { B_SAP_D3 } from '@betarena/scores-lib/types/seo-pages.js';
 import type { B_FOT_T } from '@betarena/scores-lib/types/types.main.footer.js';
+import type { B_SAP_D3 } from '@betarena/scores-lib/types/v8/preload.scores.js';
 import type { ServerLoadEvent } from '@sveltejs/kit';
 
 // #endregion ➤ 📦 Package Imports
+
+// #region ➤ 📌 VARIABLES
+
+const
+  /**
+   * @description
+   * 📝 Debugging tag.
+   */
+  strDebugModule = 'src/routes/+layout.server.ts'
+;
+
+// #endregion ➤ 📌 VARIABLES
+
+// #region ➤ ⛩️ TYPES
 
 /**
  * @author
@@ -33,7 +51,7 @@ import type { ServerLoadEvent } from '@sveltejs/kit';
  * @summary
  *  🔹 INTERFACE
  * @description
- *  📣 Target `types` for `_this_` page required at preload.
+ *  📝 Type for `_this_` page required at preload.
  */
 type IPreloadData0 =
 [
@@ -47,40 +65,118 @@ type IPreloadData0 =
  * @author
  *  @migbash
  * @summary
- *  🟥 MAIN
+ *  🔹 INTERFACE
  * @description
- *  📣 Logic for `[[lang=lang]]` route data preload.
+ *  📝 Interface for `_this_` page required at preload.
+ */
+interface IPreloadResponse
+{
+  /**
+   * @description
+   *  📝 Target `lang` identification
+   */
+  langParam: string;
+  /**
+   * @description
+   *  📝 Identify 'user-agent' object data, for target user 'device' type.
+   */
+  deviceType: string;
+  /**
+   * @description
+   * 📝 Target `urls` to be `fetched`.
+   */
+  userAgent: string | null;
+  /**
+   * @description
+   *  📝 Target `data` returned.
+   */
+  B_NAV_T?: B_NAV_T | undefined;
+  /**
+   * @description
+   *  📝 Target `data` returned.
+   */
+  B_FOT_T?: B_FOT_T | undefined;
+  /**
+   * @description
+   *  📝 Target `data` returned.
+   */
+  authTrs?: IAuthTrs | undefined;
+}
+
+// #endregion ➤ ⛩️ TYPES
+
+// ╭──────────────────────────────────────────────────────────────────────────────────╮
+// │ 🟥 │ LOGIC - MAIN                                                                │
+// ╰──────────────────────────────────────────────────────────────────────────────────╯
+
+/**
+ * @author
+ *  @migbash
+ * @summary
+ *  ♦️ MAIN
+ * @description
+ *  📝 Logic for route `src/routes/+layout.server.ts` for respective data preload.
+ * @example
+ *  [1]──────────────────────────────────────────────────────────────────
+ *  │ main
+ *  │ (
+ *  │   <-sveltekit-server-preload-event-instance->
+ *  │ );
+ *  ┣────────────────────────────────────────────────────────────────────
+ *  │ DESCRIPTION
+ *  │ : Main logic for route `src/routes/+layout.server.ts` for respective data preload.
+ *  ┣────────────────────────────────────────────────────────────────────
+ *  │ OUTPUT
+ *  │ : Returns `data` for `_this_` preload.
+ *  [X]──────────────────────────────────────────────────────────────────
+ * @param { ServerLoadEvent } event
+ *  ❗️ **REQUIRED** instance of `ServerLoadEvent` object.
  * @return { Promise < {} > }
  *  📤 Respective `data` for _this_ route.
  */
 export async function main
 (
   event: ServerLoadEvent
-): Promise < {} >
+): Promise < IPreloadResponse >
 {
+  // [🐞]
+  dlogv2
+  (
+    `🚏 checkpoint ➤ ${strDebugModule} main(..) // START`,
+    [
+      `🔹 [var] ➤ request.headers.get('user-agent') :: ${parseObject(event.request.headers)}`,
+    ]
+  );
+
   // ╭─────
   // │ NOTE:
-  // │ > testing to identify 'users ip', from within load
-  // │ > only works with deployment using '<node-server>.js'
+  // │ │: testing to identify 'users ip', from within load
+  // │ │: only works with deployment using '<node-server>.js'
   // ╰─────
   /*
     try
     {
-      // ### NOTE:
-      // ### V1 | ❌ does not appear to work, breaks platform.
-      // const response_IP = await fetch(`/getClientIP`, {
-      // const response_IP_3 = await fetch(`https://betarena-scores-platform.herokuapp.com/getClientIP`, {
+      // ╭─────
+      // │ NOTE:
+      // │ |: V1 | ❌ does not appear to work, breaks platform.
+      // ╰─────
+      // const response_IP = await fetch(`/getClientIP`, {..});
+      // const response_IP_3 = await fetch(`https://XXXX/getClientIP`, {..}
       // console.log("🔵 response_IP: ", response_IP);
 
-      // ### NOTE:
-      // ### V2 | ✅ works [?] but incorrect IP.
-      // console.log("🔵🔵🔵 event: ", event);
-      // console.log("🔵🔵🔵 event.getClientAddress(): ", event?.getClientAddress());
+      // ╭─────
+      // │ NOTE:
+      // │ |: V2 | ✅ works [?] but incorrect IP.
+      // ╰─────
+      // console.log("🔵 event: ", event);
+      // console.log("🔵 event.getClientAddress(): ", event?.getClientAddress());
 
-      // ### NOTE:
-      // ### V3 | ❓ works [?] but incorrect IP
+      // ╭─────
+      // │ NOTE:
+      // │ |: V3 | ❓ works [?] but incorrect IP
+      // ╰─────
       // const response_IP_3 = await get(`/getClientIP`)
-      // console.log("🔵🔵🔵 response_IP_3: ", response_IP_3);
+      // console.log("🔵 response_IP_3: ", response_IP_3);
     }
     catch (error)
     {
@@ -89,9 +185,11 @@ export async function main
 
     try
     {
-      // ### NOTE:
-      // ### V3 | ✅ works [?] only on when calling directly URL, not from .server.ts
-      // ### works when using the non +layout.ts/+page.ts file
+      // ╭─────
+      // │ NOTE:
+      // │ |: V3 | ✅ works [?] only on when calling directly URL, not from .server.ts
+      // │ │: works when using the non +layout.ts/+page.ts file
+      // ╰─────
       // const response_IP_2 = await get(`https://betarena-scores-platform.herokuapp.com/getClientIP`)
       // OR:
       // const response_IP_2 = await fetch(`http://betarena-scores-platform.herokuapp.com/getClientIP`, {
@@ -99,58 +197,62 @@ export async function main
       //   }
       // ).then((r) => r.json())
       // .catch((error) => { console.log(error) });
-      // console.log("🔵🔵🔵 response_IP_2: ", response_IP_2);
+      // console.log("🔵 response_IP_2: ", response_IP_2);
     }
     catch (error)
     {
       console.log(`🔴 ${error}`)
     }
   */
+
   const
     /**
      * @description
-     *  📣 Identify 'user-agent' object data, for target user 'device' type.
+     *  📝 `Data` object for target `route`.
      */
-    deviceType
-      = detectDeviceWithUA
-      (
-        event.request.headers.get('user-agent') ?? ''
-      ),
-    /**
-     * @description
-     *  📣 Target `lang` identification
-     */
-    langParam
-      = detectPlatformLanguage
-      (
-        {
-          parameterLanguage: event.params.lang,
-          cookies: event.cookies,
-          routeId: event.route.id
-        }
-      ),
-    /**
-     * @description
-     *  📣 `Data` object for target `route`.
-     */
-    response: any
+    objResponse: IPreloadResponse
       = {
-        langParam,
-        deviceType
+        langParam:
+          detectPlatformLanguage
+          (
+            {
+              parameterLanguage: event.params.lang,
+              cookies: event.cookies,
+              routeId: event.route.id
+            }
+          ),
+        deviceType:
+          detectDeviceWithUA
+          (
+            event.request.headers.get('user-agent') ?? ''
+          ),
+        userAgent: event.request.headers.get('user-agent')
       }
   ;
 
+  // ╭──────────────────────────────────────────────────────────────────────────────────╮
+  // │ 🏗️ │ PAGE DATA BUNDLING                                                          │
+  // ╰──────────────────────────────────────────────────────────────────────────────────╯
+
+  // ╭─────
+  // │ NOTE:
+  // │ |: Destruct `object`.
+  // ╰─────
   [
-    response.B_NAV_T,
-    response.B_FOT_T,
-    response.authTrs
+    objResponse.B_NAV_T,
+    objResponse.B_FOT_T,
+    objResponse.authTrs
   ] = await fetchData
   (
     event.fetch,
-    langParam
+    objResponse.langParam
   );
 
-  if (response.B_NAV_T == undefined || response.B_FOT_T == undefined)
+  // ╭─────
+  // │ NOTE:
+  // │ |: check for 'undefined' values in critical data, causing exit logic.
+  // ╰─────
+  if (objResponse.B_NAV_T == undefined || objResponse.B_FOT_T == undefined)
     preloadExitLogic
     (
       performance.now(),
@@ -163,17 +265,16 @@ export async function main
   // [🐞]
   dlogv2
   (
-    '🚏 checkpoint ➤ src/routes/(scores)/[[lang=lang]]/+page.server.ts',
+    `🚏 checkpoint ➤ ${strDebugModule} main(..) // END`,
     [
-      `🔹 [var] ➤ response :|: ${JSON.stringify(response.PAGE_DATA_SEO)}`,
-    ],
-    true
+      // `🔹 [var] ➤ objResponse :|: ${objResponse}`,
+    ]
   );
 
   // ╭─────
   // │ NOTE: | WARNING:
-  // │ > commented out due to interferences
-  // │ > with error logs and code-traces.
+  // │ │: commented out due to interferences
+  // │ │: with error logs and code-traces.
   // ╰─────
   /*
     setHeaders
@@ -183,57 +284,70 @@ export async function main
       }
     );
   */
-  return response;
+
+  return objResponse;
 }
+
+// ╭──────────────────────────────────────────────────────────────────────────────────╮
+// │ 🟦 │ LOGIC - HELPER                                                              │
+// ╰──────────────────────────────────────────────────────────────────────────────────╯
 
 /**
  * @author
  *  @migbash
  * @summary
- *  🟦 HELPER
+ *  🔷 HELPER
  * @description
- *  📣 Fetches target data for `_this_` page.
+ *  📝 Fetches target data for `_this_` page.
+ * @example
+ *  [1]──────────────────────────────────────────────────────────────────
+ *  │ fetchData
+ *  │ (
+ *  │   <-sveltekit-fetch-instance->,
+ *  │   'es'
+ *  │ );
+ *  ┣────────────────────────────────────────────────────────────────────
+ *  │ DESCRIPTION
+ *  │ : Fetches target data for `_this_` page.
+ *  ┣────────────────────────────────────────────────────────────────────
+ *  │ OUTPUT
+ *  │ : Returns `data` for `_this_` page.
+ *  [X]──────────────────────────────────────────────────────────────────
  * @param { any } fetch
- *  💠 **[required]** Target instance of `fetch` object.
- * @param { string } _lang
- *  💠 **[required]** Target `language`.
+ *  ❗️ **REQUIRED** instance of `fetch` object.
+ * @param { string } lang
+ *  ❗️ **REQUIRED** `language`.
  * @returns { Promise < IPreloadData0 > }
  *  📤 `Data` fetched.
  */
 async function fetchData
 (
   fetch: any,
-  _lang: string
+  lang: string
 ): Promise < IPreloadData0 >
 {
-  // [🐞]
-  dlog
-  (
-    '🚏 checkpoint ➤ src/routes/+layout.ts fecthData(..)'
-  );
-
   const
     /**
      * @description
-     *  📣 Target `urls` to be `fetched`.
+     *  📝 Target `urls` to be `fetched`.
      */
-    urls0: string[]
+    listUrls
       = [
-        `/api/data/main/navbar?lang=${_lang}&decompress`,
-        `/api/data/main/footer?lang=${_lang}&decompress`,
-        `/api/hasura/_main_/auth?lang=${_lang}`,
+        `/api/data/main/navbar?lang=${lang}&decompress`,
+        `/api/data/main/footer?lang=${lang}&decompress`,
+        `/api/hasura/_main_/auth?lang=${lang}`,
       ],
     /**
      * @description
-     *  📣 Target `data` returned.
+     *  📝 Target `data` returned.
      */
-    data0
+    dataRes0
       = await promiseUrlsPreload
       (
-        urls0,
+        listUrls,
         fetch
       ) as IPreloadData0
   ;
 
-  return data0;
+  return dataRes0;
 }
