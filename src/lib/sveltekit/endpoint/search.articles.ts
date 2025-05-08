bdf9c56a2a6cc23bb92cdcb38aa304e4dd690a12
Ivan Izobov <izobov9@gmail.com>
2025-05-08 17:11:17 -0100

    fix: refactor language extraction in ArticlesSearchEndpoint for clarity

--- src/lib/sveltekit/endpoint/search.articles.ts
+++ src/lib/sveltekit/endpoint/search.articles.ts
@@ -6,6 +6,7 @@ export const ArticlesSearchEndpoint: RequestHandler = async ({url, locals}) => {
   const page = url.searchParams.get('page') || '0';
   const limit = url.searchParams.get('limit') || '10';
   const offset = parseInt(page) * parseInt(limit);
-  const result = await searchArticles({text, offset, limit: parseInt(limit), lang: locals.user?.lang || "en"})
+  const lang = locals.user ? JSON.parse(locals.user).lang : "en";
+  const result = await searchArticles({ text, offset, limit: parseInt(limit), lang });
   return json({articles: result})
 };
