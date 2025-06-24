4d00d0278a4d788f282c391f8955e4479702b997
Ivan Izobov <izobov9@gmail.com>
2025-06-24 13:35:00 +0100

    feat: android banner

--- src/lib/components/ui/assets/x-close.svelte
+++ src/lib/components/ui/assets/x-close.svelte
@@ -1,11 +1,12 @@
 <script>
   export let color = "var(--colors-foreground-fg-quinary)";
+  export let size = 24
 </script>
 
 <svg
   xmlns="http://www.w3.org/2000/svg"
-  width="24"
-  height="24"
+  width={size}
+  height={size}
   viewBox="0 0 24 24"
   fill="none"
 >
