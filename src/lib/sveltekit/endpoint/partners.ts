c9d956d6fe13770bbba9cb7a258272d70646a1eb
Ivan Izobov <izobov9@gmail.com>
2025-07-09 07:23:18 +0100

    fix: add default data for partners

--- src/lib/sveltekit/endpoint/partners.ts
+++ src/lib/sveltekit/endpoint/partners.ts
@@ -1,43 +1,46 @@
 import { json, RequestHandler } from "@sveltejs/kit";
-import {getPartners, getPartnersSubmissions, insertPartnerSubmission} from "@betarena/scores-lib/dist/functions/v8/partners.js"
+import {
+  getPartners,
+  getPartnersSubmissions,
+  insertPartnerSubmission,
+} from "@betarena/scores-lib/dist/functions/v8/partners.js";
 
 export const GetPartners: RequestHandler = async ({ url }) => {
-  const geo = url.searchParams.get("geo");
-  if (!geo) return json({partners: []})
-  const data = await getPartners({ geo });
-  return json ({ partners: data});
+  const geo = url.searchParams.get("geo") || "en";
+  const [target, default_data] = await Promise.all([getPartners({ geo }), getPartners({ geo: "en" })]);
+  return json({ partners: target?.length ? target : default_data });
 };
-
 
 export const GetPartnersSubmissions: RequestHandler = async ({ url }) => {
   const uid = url.searchParams.get("uid");
-  if (!uid) return json({partners: []})
+  if (!uid) return json({ partners: [] });
 
-  try
-  {
-
-    const data = await getPartnersSubmissions({  uid });
+  try {
+    const data = await getPartnersSubmissions({ uid });
     return json({ partners_submissions: data });
-  } catch (e)
-  {
+  } catch (e) {
     console.log("Partners Submissions", e);
-    return json({ });
+    return json({});
   }
-}
+};
 
-export const PostPartnerSubmission: RequestHandler = async ({locals: {user}, request}) =>
-{
-  const { partner, input, wallet } = await request.json()
-  if (!user.uid)
-  {
-    return json({ok: false}, { status: 501 });
+export const PostPartnerSubmission: RequestHandler = async ({
+  locals: { user },
+  request,
+}) => {
+  const { partner, input, wallet } = await request.json();
+  if (!user.uid) {
+    return json({ ok: false }, { status: 501 });
   }
-  try
-  {
-    const submission = await insertPartnerSubmission({partner_id: partner.id, user_id: user.uid, status: "pending", submitted_username_or_email: input })
-    return json({submission, ok: true}, {status: 200})
-  } catch (e)
-  {
-    return json({ok: false}, { status: 500 });
+  try {
+    const submission = await insertPartnerSubmission({
+      partner_id: partner.id,
+      user_id: user.uid,
+      status: "pending",
+      submitted_username_or_email: input,
+    });
+    return json({ submission, ok: true }, { status: 200 });
+  } catch (e) {
+    return json({ ok: false }, { status: 500 });
   }
-}
+};
