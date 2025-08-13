b14e4ad99eb25de25e67b4f4eabb4daeaf6014ae
Ivan Izobov <izobov9@gmail.com>
2025-08-13 13:36:20 -0100

    fix: change step

--- src/lib/components/section/login/login-store.ts
+++ src/lib/components/section/login/login-store.ts
@@ -6,7 +6,7 @@ export const loginStore = writable({
     isLogin: false,
     password: "",
     name: "",
-    currentStep: 3,
+    currentStep: 0,
     avatar: "",
     phoneNumber: "",
     confirmationResult: null as ConfirmationResult | null,
