8b55dc7ab608a4f9251ac922fb576a17b0e25702
Ivan Izobov <izobov9@gmail.com>
2025-10-30 10:50:21 -0100

    feat: insufficient balance

--- src/lib/components/section/authors/common_ui/articles/TipsModal.svelte
+++ src/lib/components/section/authors/common_ui/articles/TipsModal.svelte
@@ -38,6 +38,8 @@
   import { getRates } from "$lib/utils/web3.js";
   import { type DotLottie, DotLottieSvelte } from "@lottiefiles/dotlottie-svelte";
   import { modalStore } from "$lib/store/modal.js";
+  import { walletStore } from "$lib/store/wallets.js";
+  import { showDepositModal } from "$lib/components/page/profile/deposit/showDeposit.js";
   // #endregion ➤ 📦 Package Imports
 
   // #region ➤ 📌 VARIABLES
@@ -58,6 +60,7 @@
 
   $: ({ viewportType } = $session);
   $: user = $userSettings.user?.scores_user_data;
+  $: insufficientAmount = $walletStore.spending.available < 1;
   // #endregion ➤ 📌 VARIABLES
 
   // #region ➤ 🛠️ METHODS
@@ -88,6 +91,10 @@
     setTimeout(() => {
       $modalStore.show = false;
     }, 700)
+  }
+
+  function cancel() {
+    $modalStore.show = false;
   }
   // #endregion ➤ 🛠️ METHODS
 
@@ -123,100 +130,123 @@
   out:chooseTransition={{ easing: cubicIn, out: true }}
 >
   <div class="header">
-    <div class="icon-wrapper">
-      <FeaturedIcon color="brand" type="gradient" size="lg">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="none"
-        >
-          <path
-            d="M9.99984 12.5C7.23841 12.5 4.99984 10.2614 4.99984 7.50001V2.87038C4.99984 2.5255 4.99984 2.35307 5.05009 2.21499C5.13434 1.98352 5.31668 1.80118 5.54815 1.71693C5.68623 1.66667 5.85867 1.66667 6.20354 1.66667H13.7961C14.141 1.66667 14.3134 1.66667 14.4515 1.71693C14.683 1.80118 14.8653 1.98352 14.9496 2.21499C14.9998 2.35307 14.9998 2.5255 14.9998 2.87038V7.50001C14.9998 10.2614 12.7613 12.5 9.99984 12.5ZM9.99984 12.5V15M14.9998 3.33334H17.0832C17.4715 3.33334 17.6656 3.33334 17.8187 3.39677C18.0229 3.48135 18.1852 3.64358 18.2697 3.84777C18.3332 4.00091 18.3332 4.19505 18.3332 4.58334V5.00001C18.3332 5.77498 18.3332 6.16247 18.248 6.48039C18.0168 7.34312 17.3429 8.01699 16.4802 8.24815C16.1623 8.33334 15.7748 8.33334 14.9998 8.33334M4.99984 3.33334H2.9165C2.52822 3.33334 2.33408 3.33334 2.18093 3.39677C1.97674 3.48135 1.81452 3.64358 1.72994 3.84777C1.6665 4.00091 1.6665 4.19505 1.6665 4.58334V5.00001C1.6665 5.77498 1.6665 6.16247 1.75169 6.48039C1.98286 7.34312 2.65673 8.01699 3.51946 8.24815C3.83737 8.33334 4.22486 8.33334 4.99984 8.33334M6.20354 18.3333H13.7961C14.0007 18.3333 14.1665 18.1675 14.1665 17.963C14.1665 16.3266 12.8399 15 11.2035 15H8.79613C7.15973 15 5.83317 16.3266 5.83317 17.963C5.83317 18.1675 5.99899 18.3333 6.20354 18.3333Z"
-            stroke="currentColor"
-            stroke-width="1.336"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </FeaturedIcon>
-    </div>
-    <div class="text-wrapper">
-      <div class="title">Give an Award</div>
-      <div class="awards-flow">
-          <div class="sportstack-wrapper award-item">
-            <div class="avatar-wrapper">
-
-              <SportstackAvatar size="lg" src={sportstack.data?.avatar}/>
-              <div class="connector-wrapper">
-                <svg xmlns="http://www.w3.org/2000/svg" width="2" height="67" viewBox="0 0 2 67" fill="none">
-                  <path d="M1 1L1 65.0115" stroke="#525252" stroke-width="2" stroke-linecap="round" stroke-dasharray="0.1 6"/>
-                </svg>
-              </div>
-            </div>
-            <div class="award-content">
-              <div class="award-candidate">
-                <div class="name">{sportstack.data?.username}</div>
-                <div class="type">Sportstack</div>
-              </div>
-              <div class="amount">
-                <div class="bta-icon">
-                  <img src={bta_icon} alt="BTA Icon" width="40" height="40"/>
-                </div>
-                <div class="description">
-                  <div class="numbers">0.5 BTA
-                    {#if $session.btaUsdRate}
-                    <span class="usd">{convertToUsd(0.5)}$</span>
-                    {/if}
-                  </div>
-                  <div class="text-secondary">
-                    goes to the publication
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="award-item">
-            <Avatar size="lg" src={user?.profile_photo}/>
-            <div class="award-content">
-              <div class="award-candidate">
-                <div class="name">{user?.name}</div>
-                <div class="type">@{user?.username}</div>
-              </div>
-              <div class="amount">
-                <div class="bta-icon">
-                  <img src={bta_icon} alt="BTA Icon" width="40" height="40"/>
-                </div>
-                <div class="description">
-                  <div class="numbers">0.5 BTA
-                    {#if $session.btaUsdRate}
-                       <span class="usd">{convertToUsd(0.5)}$</span>
-                      {/if}
-                  </div>
-                  <div class="text-secondary">
-                    returns to your Rewards wallet
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+    {#if !insufficientAmount}
+      <div class="icon-wrapper">
+        <FeaturedIcon color="brand" type="gradient" size="lg">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+          >
+            <path
+              d="M9.99984 12.5C7.23841 12.5 4.99984 10.2614 4.99984 7.50001V2.87038C4.99984 2.5255 4.99984 2.35307 5.05009 2.21499C5.13434 1.98352 5.31668 1.80118 5.54815 1.71693C5.68623 1.66667 5.85867 1.66667 6.20354 1.66667H13.7961C14.141 1.66667 14.3134 1.66667 14.4515 1.71693C14.683 1.80118 14.8653 1.98352 14.9496 2.21499C14.9998 2.35307 14.9998 2.5255 14.9998 2.87038V7.50001C14.9998 10.2614 12.7613 12.5 9.99984 12.5ZM9.99984 12.5V15M14.9998 3.33334H17.0832C17.4715 3.33334 17.6656 3.33334 17.8187 3.39677C18.0229 3.48135 18.1852 3.64358 18.2697 3.84777C18.3332 4.00091 18.3332 4.19505 18.3332 4.58334V5.00001C18.3332 5.77498 18.3332 6.16247 18.248 6.48039C18.0168 7.34312 17.3429 8.01699 16.4802 8.24815C16.1623 8.33334 15.7748 8.33334 14.9998 8.33334M4.99984 3.33334H2.9165C2.52822 3.33334 2.33408 3.33334 2.18093 3.39677C1.97674 3.48135 1.81452 3.64358 1.72994 3.84777C1.6665 4.00091 1.6665 4.19505 1.6665 4.58334V5.00001C1.6665 5.77498 1.6665 6.16247 1.75169 6.48039C1.98286 7.34312 2.65673 8.01699 3.51946 8.24815C3.83737 8.33334 4.22486 8.33334 4.99984 8.33334M6.20354 18.3333H13.7961C14.0007 18.3333 14.1665 18.1675 14.1665 17.963C14.1665 16.3266 12.8399 15 11.2035 15H8.79613C7.15973 15 5.83317 16.3266 5.83317 17.963C5.83317 18.1675 5.99899 18.3333 6.20354 18.3333Z"
+              stroke="currentColor"
+              stroke-width="1.336"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </FeaturedIcon>
       </div>
+    {/if}
+    <div class="text-wrapper">
+      <div class="title">
+      {#if insufficientAmount}
+        Insufficient Balance
+      {:else}
+         Give an Award
+      {/if}
+
+      </div>
+      {#if insufficientAmount}
+        <div class="warning-text">
+          <p class="warning-main">You need at least 1 BTA in your Balance wallet to award this post. </p>
+          <p class="warning-support">You always receive half of the award back to your Rewards wallet.</p>
+        </div>
+      {:else}
+         <div class="awards-flow">
+             <div class="sportstack-wrapper award-item">
+               <div class="avatar-wrapper">
+
+                 <SportstackAvatar size="lg" src={sportstack.data?.avatar}/>
+                 <div class="connector-wrapper">
+                   <svg xmlns="http://www.w3.org/2000/svg" width="2" height="67" viewBox="0 0 2 67" fill="none">
+                     <path d="M1 1L1 65.0115" stroke="#525252" stroke-width="2" stroke-linecap="round" stroke-dasharray="0.1 6"/>
+                   </svg>
+                 </div>
+               </div>
+               <div class="award-content">
+                 <div class="award-candidate">
+                   <div class="name">{sportstack.data?.username}</div>
+                   <div class="type">Sportstack</div>
+                 </div>
+                 <div class="amount">
+                   <div class="bta-icon">
+                     <img src={bta_icon} alt="BTA Icon" width="40" height="40"/>
+                   </div>
+                   <div class="description">
+                     <div class="numbers">0.5 BTA
+                       {#if $session.btaUsdRate}
+                       <span class="usd">{convertToUsd(0.5)}$</span>
+                       {/if}
+                     </div>
+                     <div class="text-secondary">
+                       goes to the publication
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             </div>
+             <div class="award-item">
+               <Avatar size="lg" src={user?.profile_photo}/>
+               <div class="award-content">
+                 <div class="award-candidate">
+                   <div class="name">{user?.name}</div>
+                   <div class="type">@{user?.username}</div>
+                 </div>
+                 <div class="amount">
+                   <div class="bta-icon">
+                     <img src={bta_icon} alt="BTA Icon" width="40" height="40"/>
+                   </div>
+                   <div class="description">
+                     <div class="numbers">0.5 BTA
+                       {#if $session.btaUsdRate}
+                          <span class="usd">{convertToUsd(0.5)}$</span>
+                         {/if}
+                     </div>
+                     <div class="text-secondary">
+                       returns to your Rewards wallet
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             </div>
+         </div>
+      {/if}
     </div>
   </div>
   <div class="footer">
-    <div class="confetti">
-      <DotLottieSvelte  dotLottieRefCallback={(ref) => dotLottie = ref } src="/assets/lottie/Confetti.lottie"   />
-    </div>
-    <Button type="primary" full={true} size="lg" on:click={confirm}>Share 1BTA
-      {#if $session.btaUsdRate}
-         <span class="button-usd">({convertToUsd(1)}$)</span>
-      {/if}
-    </Button>
-    <Button type="secondary" full={true} size="lg" on:click={() => $modalStore.show = false}>Cancel</Button>
-    <div class="checkbox-wrapp">
-      <Checkbox title="Don't show again" />
-    </div>
+    {#if insufficientAmount}
+        <div class="insufficient-buttons">
+          <Button type="secondary" on:click={cancel} full={true}>Cancel</Button>
+          <Button full={true} on:click={showDepositModal}>Buy BTA </Button>
+        </div>
+    {:else}
+       <div class="confetti">
+         <DotLottieSvelte  dotLottieRefCallback={(ref) => dotLottie = ref } src="/assets/lottie/Confetti.lottie"   />
+       </div>
+       <Button type="primary" full={true} size="lg" on:click={confirm}>Share 1BTA
+         {#if $session.btaUsdRate}
+            <span class="button-usd">({convertToUsd(1)}$)</span>
+         {/if}
+       </Button>
+       <Button type="secondary" full={true} size="lg" on:click={cancel}>Cancel</Button>
+       <div class="checkbox-wrapp">
+         <Checkbox title="Don't show again" />
+       </div>
+    {/if}
   </div>
 </div>
 
@@ -408,6 +438,33 @@
             color: var(--colors-text-text-secondary-700, #fbfbfb) !important;
           }
         }
+
+        .warning-text {
+          display: flex;
+          flex-direction: column;
+          gap: var(--spacing-lg, 12px);
+
+          .warning-main {
+            color: var(--colors-text-text-secondary-700, #FBFBFB);
+
+            /* Text sm/Regular */
+            font-family: var(--font-family-font-family-body, Roboto);
+            font-size: var(--font-size-text-sm, 14px);
+            font-style: normal;
+            font-weight: 400;
+            line-height: var(--line-height-text-sm, 20px); /* 142.857% */
+          }
+          .warning-support {
+            color: var(--colors-text-text-tertiary-600, #8C8C8C);
+
+            /* Text sm/Regular */
+            font-family: var(--font-family-font-family-body, Roboto);
+            font-size: var(--font-size-text-sm, 14px);
+            font-style: normal;
+            font-weight: 400;
+            line-height: var(--line-height-text-sm, 20px);
+          }
+        }
       }
     }
     .footer {
@@ -442,6 +499,12 @@
         font-weight: 600;
         line-height: var(--line-height-text-xs, 18px);
       }
+
+      .insufficient-buttons {
+        width: 100%;
+        display: flex;
+        gap: var(--spacing-lg, 12px);
+      }
     }
 
     &.mobile {
@@ -462,6 +525,10 @@
         padding-top: var(--spacing-3xl, 24px);
         flex-direction: column;
         align-items: start;
+
+        .insufficient-buttons {
+          flex-direction: column-reverse;
+        }
       }
       padding-bottom: var(--spacing-3xl, 24px);
     }
