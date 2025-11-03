e2a1e8135647a9e21fbf848dd3abd06eacb767a1
Ivan Izobov <izobov9@gmail.com>
2025-11-03 10:40:59 -0100

    fix: bta rates

--- src/lib/utils/web3.ts
+++ src/lib/utils/web3.ts
@@ -12,9 +12,9 @@
 
 // #region ➤ 📦 Package Imports
 
-import { get } from '$lib/api/utils.js';
-import type { Chain } from '@web3modal/scaffold-utils/dist/types/src/EthersTypesUtil.js';
-import type { Writable } from 'svelte/store';
+import { get } from "$lib/api/utils.js";
+import type { Chain } from "@web3modal/scaffold-utils/dist/types/src/EthersTypesUtil.js";
+import type { Writable } from "svelte/store";
 
 // #endregion ➤ 📦 Package Imports
 
@@ -23,46 +23,31 @@ import type { Writable } from 'svelte/store';
  *  - 📣 Object for target `web3` chains.
  *  - 📣 Used in conjuction with `Standard Web3 Method` configuration.
  */
-export const chainObject: unknown
-= {
-  polygon_mumbai:
-  {
-    chainId: '0x13881',
-    chainName: 'Mumbai Testnet',
-    rpcUrls:
-    [
-      'https://polygon-mumbai.g.alchemy.com/v2/0zWtf5I24NBkM826G-VJFiYkkGoC5atJ'
+export const chainObject: unknown = {
+  polygon_mumbai: {
+    chainId: "0x13881",
+    chainName: "Mumbai Testnet",
+    rpcUrls: [
+      "https://polygon-mumbai.g.alchemy.com/v2/0zWtf5I24NBkM826G-VJFiYkkGoC5atJ",
     ],
-    nativeCurrency:
-    {
-      name: 'MATIC',
-      symbol: 'MATIC',
-      decimals: 18
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "MATIC",
+      decimals: 18,
     },
-    blockExplorerUrls:
-    [
-      'https://mumbai.polygonscan.com'
-    ]
+    blockExplorerUrls: ["https://mumbai.polygonscan.com"],
   },
-  polygon_mainnet:
-  {
-    chainId: '0x89',
-    chainName: 'Polygon Mainnet',
-    rpcUrls:
-    [
-      'https://polygon-rpc.com/'
-    ],
-    nativeCurrency:
-    {
-      name: 'MATIC',
-      symbol: 'MATIC',
-      decimals: 18
+  polygon_mainnet: {
+    chainId: "0x89",
+    chainName: "Polygon Mainnet",
+    rpcUrls: ["https://polygon-rpc.com/"],
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "MATIC",
+      decimals: 18,
     },
-    blockExplorerUrls:
-    [
-      'https://polygonscan.com'
-    ]
-  }
+    blockExplorerUrls: ["https://polygonscan.com"],
+  },
 };
 
 /**
@@ -70,53 +55,50 @@ export const chainObject: unknown
  *  - 📣 Object for target `web3` chains.
  *  - 📣 Used in conjuction with `WalletConnect` configuration.
  */
-export const chainObjectWalletConnect: Record < 'ethereum' | 'polygon_mumbai' | 'polygon_mainnet', Chain >
-= {
-  ethereum:
-  {
+export const chainObjectWalletConnect: Record<
+  "ethereum" | "polygon_mumbai" | "polygon_mainnet",
+  Chain
+> = {
+  ethereum: {
     chainId: 1,
-    name: 'Ethereum',
-    currency: 'ETH',
-    explorerUrl: 'https://etherscan.io',
-    rpcUrl: 'https://cloudflare-eth.com'
+    name: "Ethereum",
+    currency: "ETH",
+    explorerUrl: "https://etherscan.io",
+    rpcUrl: "https://cloudflare-eth.com",
   },
-  polygon_mumbai:
-  {
+  polygon_mumbai: {
     chainId: 80001,
-    name: 'Mumbai Testnet',
-    currency: 'MATIC',
-    explorerUrl: 'https://mumbai.polygonscan.com',
-    rpcUrl: 'https://polygon-mumbai.g.alchemy.com/v2/0zWtf5I24NBkM826G-VJFiYkkGoC5atJ'
+    name: "Mumbai Testnet",
+    currency: "MATIC",
+    explorerUrl: "https://mumbai.polygonscan.com",
+    rpcUrl:
+      "https://polygon-mumbai.g.alchemy.com/v2/0zWtf5I24NBkM826G-VJFiYkkGoC5atJ",
   },
-  polygon_mainnet:
-  {
+  polygon_mainnet: {
     chainId: 137,
-    name: 'Polygon',
-    currency: 'MATIC',
-    explorerUrl: 'https://polygonscan.com',
-    rpcUrl: 'https://polygon-rpc.com'
-  }
+    name: "Polygon",
+    currency: "MATIC",
+    explorerUrl: "https://polygonscan.com",
+    rpcUrl: "https://polygon-rpc.com",
+  },
 };
 
 let timer: ReturnType<typeof setInterval>;
-export async function getRates(store: Writable<any>)
-{
-
-   const res = await get("/api/data/bta-rates") as {
-      data?:      { [key: string]: any };
-      symbol?:    string;
-      timestamp?: string;
-      [property: string]: any;
-    }
-    if(res) {
-      store.update((state) => {
-        state.btaUsdRate = res.success.data.intBtaEstimate;
-        return state;
-      });
-      return
-    }
-    timer = setTimeout(() => {
-      getRates(store);
-    }, 10000);
+export async function getRates(store: Writable<any>) {
+  const res = (await get("/api/data/bta-rates")) as {
+    data?: { [key: string]: any };
+    symbol?: string;
+    timestamp?: string;
+    [property: string]: any;
+  };
+  if (res) {
+    store.update((state) => {
+      state.btaUsdRate = res.bta_rates.data.price_in.usd;
+      return state;
+    });
+    return;
+  }
+  timer = setTimeout(() => {
+    getRates(store);
+  }, 10000);
 }
-
